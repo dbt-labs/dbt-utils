4337b23576696b9c29d15aacc2becca7dc0c5ba9
Drew Banin <drew@fishtownanalytics.com>
2017-08-21 13:38:58 -0400

    fix dateadd for postgres

--- macros/cross_db_utils/dateadd.sql
+++ macros/cross_db_utils/dateadd.sql
@@ -27,6 +27,6 @@
 
 {% macro postgres__dateadd(datepart, interval, from_date_or_timestamp) %}
 
-    from_date_or_timestamp + interval '{{ interval }} {{ datepart }}'
+    {{ from_date_or_timestamp }} + ((interval '1 {{ datepart }}') * ({{ interval }}))
 
 {% endmacro %}

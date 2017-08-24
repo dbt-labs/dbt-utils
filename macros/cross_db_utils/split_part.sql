f644825c4f23f7ee753064bb7b2405c705f2efe9
Tristan Handy <jthandy.gmail.com>
2017-08-24 12:30:08 -0400

    removed notes to self

--- macros/cross_db_utils/split_part.sql
+++ macros/cross_db_utils/split_part.sql
@@ -1,24 +1,3 @@
-/*
-bigquery:
-SELECT FIRST(SPLIT(path, '/')) part1,
-       NTH(2, SPLIT(path, '/')) part2,
-       NTH(3, SPLIT(path, '/')) part3
-FROM (SELECT "/a/b/aaaa?c" path)
-
-
-redshift:
-SPLIT_PART(string, delimiter, part)
-
-postgres:
-split_part(string text, delimiter text, field int)
-
-snowflake:
-SPLIT_PART(<string>, <delimiter>, <partNr>)
-
-looks like there is a clear default and bq is different
-*/
-
-
 {% macro split_part(string_text, delimiter_text, part_number) %}
   {{ adapter_macro('split_part', string_text, delimiter_text, part_number) }}
 {% endmacro %}

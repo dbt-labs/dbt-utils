cacd6beed66998172ea3f92d3a0aef342dff9d2d
David Wallace <dwallace0723@gmail.com>
2018-01-04 08:07:56 -0800

    address asymmetrical nature of except operator

--- macros/schema_tests/cardinality_equality.sql
+++ macros/schema_tests/cardinality_equality.sql
@@ -14,15 +14,31 @@ select
   count(1) as num_rows
 from {{ to }}
 group by 1
+),
+
+except_a as (
+  select *
+  from table_a
+  except
+  select *
+  from table_b
+),
+
+except_b as (
+  select *
+  from table_b
+  except
+  select *
+  from table_a
 )
 
 select count(1)
 from (
-select *
-from table_a
-except
-select *
-from table_b
+  select *
+  from except_a
+  union all
+  select *
+  from except_b
 )
 
 {% endmacro %}

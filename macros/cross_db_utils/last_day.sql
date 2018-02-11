428c5961b020cef56e9d1455ae9dc44a3de1da89
Tristan Handy <jthandy@gmail.com>
2018-02-11 14:51:12 -0500

    made code more dry

--- macros/cross_db_utils/last_day.sql
+++ macros/cross_db_utils/last_day.sql
@@ -8,30 +8,27 @@ testing is required to validate that it will work on other dateparts.
 {% endmacro %}
 
 
-{% macro default__last_day(date, datepart) -%}
+{%- macro default_last_day(date, datepart) -%}
     cast(
         {{dbt_utils.dateadd('day', '-1',
         dbt_utils.dateadd(datepart, '1', dbt_utils.date_trunc(datepart, date))
         )}}
         as date)
+{%- endmacro -%}
+
+
+{% macro default__last_day(date, datepart) -%}
+    {{dbt_utils.default_last_day(date, datepart)}}
 {%- endmacro %}
 
 
 {% macro postgres__last_day(date, datepart) -%}
 
     {%- if datepart == 'quarter' -%}
-
     {{ exceptions.raise_compiler_error(
         "dbt_utils.last_day is not supported for datepart 'quarter' on this adapter") }}
-
     {%- else -%}
-
-    cast(
-        {{dbt_utils.dateadd('day', '-1',
-        dbt_utils.dateadd(datepart, '1', dbt_utils.date_trunc(datepart, date))
-        )}}
-        as date)
-
+    {{dbt_utils.default_last_day(date, datepart)}}
     {%- endif -%}
 
 {%- endmacro %}

1acd039a31fe227ed862feb4b55aed8d68007d03
Tristan Handy <jthandy@gmail.com>
2018-05-22 16:29:26 -0400

    added functionality to get current timestamp in utc

--- macros/cross_db_utils/current_timestamp.sql
+++ macros/cross_db_utils/current_timestamp.sql
@@ -3,13 +3,41 @@
 {% endmacro %}
 
 {% macro default__current_timestamp() %}
-    current_timestamp()
+    current_timestamp
 {% endmacro %}
 
 {% macro redshift__current_timestamp() %}
-    current_timestamp::timestamp
+    current_timestamp::{{dbt_utils.type_timestamp()}}
 {% endmacro %}
 
 {% macro postgres__current_timestamp() %}
-    current_timestamp::timestamp
+    current_timestamp::{{dbt_utils.type_timestamp()}}
 {% endmacro %}
+
+
+
+
+
+
+{% macro current_timestamp_in_utc() %}
+  {{ adapter_macro('dbt_utils.current_timestamp_in_utc') }}
+{% endmacro %}
+
+{% macro default__current_timestamp_in_utc() %}
+    {{dbt_utils.current_timestamp()}}
+{% endmacro %}
+
+{% macro snowflake__current_timestamp_in_utc() %}
+    convert_timezone('UTC', {{dbt_utils.current_timestamp()}})::{{dbt_utils.type_timestamp()}}
+{% endmacro %}
+
+{% macro postgres__current_timestamp_in_utc() %}
+    (current_timestamp at time zone 'utc')::{{dbt_utils.type_timestamp()}}
+{% endmacro %}
+
+
+
+
+
+
+

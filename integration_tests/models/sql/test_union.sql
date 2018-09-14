6a3e80ce0418e24d0802f51e7a8afababef03005
Drew Banin <drew@fishtownanalytics.com>
2018-09-14 15:52:50 -0400

    fix for different dev/ci schema names

--- integration_tests/models/sql/test_union.sql
+++ integration_tests/models/sql/test_union.sql
@@ -1,6 +1,8 @@
 
-{{ dbt_utils.union_tables([
-        ref('data_union_table_1'),
-        ref('data_union_table_2')]
-) }} 
+select
+    id,
+    name,
+    favorite_color
+
+from {{ ref('test_union_base') }}
 

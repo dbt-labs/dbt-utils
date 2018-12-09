61d6f9dda3bf68f600ca69f2ea469ee87743ee32
Alexa Diaz <axela.zaid@gmail.com>
2018-12-08 19:47:37 -0500

    created 'right' function macro; added tests

---
+++ macros/cross_db_utils/right.sql
@@ -0,0 +1,14 @@
+{% macro right(string_text, length_expression) -%}
+    {{ adapter_macro('dbt_utils.right', string_text, length_expression) }}
+{% endmacro %}
+
+
+{% macro default__right(string_text, length_expression) %}
+
+    right(
+        {{ string_text }},
+        {{ length_expression }}
+    )
+    
+
+{% endmacro %}

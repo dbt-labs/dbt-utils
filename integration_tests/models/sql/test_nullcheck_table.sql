99443ea2f4decea884f6f72fb6f0c098201ed3dd
Alexa Diaz <axela.zaid@gmail.com>
2018-12-17 23:09:03 -0500

    updated test model for snowflake compatibility

--- integration_tests/models/sql/test_nullcheck_table.sql
+++ integration_tests/models/sql/test_nullcheck_table.sql
@@ -11,6 +11,17 @@ with nulled as (
 
 )
 
+{% if target.type == 'snowflake' %}
+
+select
+    field_1::varchar as field_1,
+    field_2::varchar as field_2,
+    field_3::varchar as field_3
+
+from nulled
+
+{% else %}
+
 select
 
     {{ dbt_utils.safe_cast('field_1',
@@ -26,3 +37,5 @@ select
     )}} as field_3
 
 from nulled
+
+{% endif %}

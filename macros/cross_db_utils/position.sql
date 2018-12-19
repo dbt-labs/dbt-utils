2d09f00a44584968fc92a1673d9b973b7862c908
Alexa Diaz <axela.zaid@gmail.com>
2018-12-19 13:40:41 -0500

    updated position macro; added seed file; created tests

--- macros/cross_db_utils/position.sql
+++ macros/cross_db_utils/position.sql
@@ -1,12 +1,22 @@
-{% macro position(substring, string_text) -%}
-    {{ adapter_macro('dbt_utils.position', substring, string_text) }}
+{% macro position(substring_text, string_text) -%}
+    {{ adapter_macro('dbt_utils.position', substring_text, string_text) }}
 {% endmacro %}
 
 
-{% macro default__position(substring, string_text) %}
+{% macro default__position(substring_text, string_text) %}
 
     position(
-        {{ substring }} in {{ string_text }}
+        {{ substring_text }} in {{ string_text }}
+    )
+    
+{%- endmacro -%}
+
+{% macro bigquery__position(substring_text, string_text) %}
+
+    strpos(
+        {{ string_text }},
+        {{ substring_text }}
+        
     )
     
 {%- endmacro -%}

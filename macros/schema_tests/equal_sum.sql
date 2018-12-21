8c30dc82927461937a2d423ceb34c349ca4ac1ad
Claus Herther <claus@calogica.com>
2018-12-21 14:31:18 -0800

    Changes signature

--- macros/schema_tests/equal_sum.sql
+++ macros/schema_tests/equal_sum.sql
@@ -1,14 +1,4 @@
-{% macro test_equal_sum(model) %}
-
-{% set column_name = kwargs.get('column_name', kwargs.get('arg')) %}
-{% set compare_model = kwargs.get('compare_model', kwargs.get('arg')) %}
-{% set compare_column_name = kwargs.get('compare_column', kwargs.get('arg')) %}
-
-{#-- Prevent querying of db in parsing mode. This works because this macro does not create any new refs. #}
-{%- if not execute -%}
-    {{ return('') }}
-{% endif %}
-
+{% macro test_equal_sum(model, column_name, compare_model, compare_column) %}
 with a as (
 
     select sum({{ column_name }}) as sum_a from {{ model }}
@@ -16,7 +6,7 @@ with a as (
 ),
 b as (
 
-    select sum({{ compare_column_name }}) as sum_b from {{ compare_model }}
+    select sum({{ compare_column }}) as sum_b from {{ compare_model }}
 
 ),
 final as (

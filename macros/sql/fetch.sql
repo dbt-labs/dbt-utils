8bee3c53435bce199c63d0d526e925a7a6c403ba
Chris Lawliss <chrislawliss@gmail.com>
2019-03-08 14:27:58 -0500

    separated macros into individual files changed macro reference 'fetch' to 'dbt_utils.fetch'

---
+++ macros/sql/fetch.sql
@@ -0,0 +1,21 @@
+{% macro fetch(sql) %}
+{# This macro returns a dictionary of the form {column_name: [results]} #}
+
+    {%- call statement('get_sql_results', fetch_result=True) -%}
+
+        {{sql}}
+
+    {%- endcall -%}
+    
+    {% set sql_results={} %}
+    
+    {%- if execute -%}
+        {% set sql_results_table = load_result('get_sql_results').table.columns %}
+        {% for column, column_values in sql_results_table.items() %}
+            {% do sql_results.update({column: column_values.values()}) %}
+        {% endfor %}
+    {%- endif -%}
+    
+    {{ return(sql_results) }}
+    
+{% endmacro %}

90d9cef0201c52edc64bbaf3a66f865baf68de54
Chris Lawliss <chrislawliss@gmail.com>
2019-05-03 14:54:41 -0400

    make macro only run on execute, add data tests for get_column_from_sql macro

--- macros/sql/get_column_from_sql.sql
+++ macros/sql/get_column_from_sql.sql
@@ -1,3 +1,9 @@
 {% macro get_column_from_sql(sql, column_name) %}
+
+{% if execute %}
     {{ return(dbt_utils.fetch(sql)[column_name]) }}
+{% else %}
+    {{return([])}}
+{% endif %}
+
 {% endmacro %}

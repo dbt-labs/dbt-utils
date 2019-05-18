5ffc95196e9cefdaaf70d2ac4b477a533052355a
Emilie Lima Schario <14057155+emilieschario@users.noreply.github.com>
2019-05-18 08:15:29 -0400

    update macro
    based on work in https://gitlab.com/gitlab-data/analytics/merge_requests/958/diffs

--- macros/sql/get_column_values.sql
+++ macros/sql/get_column_values.sql
@@ -1,4 +1,3 @@
-
 {#
 This macro fetches the unique values for `column` in the table `table`
 
@@ -11,19 +10,47 @@ Returns:
     A list of distinct values for the specified columns
 #}
 
-{% macro get_column_values(table, column, max_records=none) -%}
+{% macro get_columns(table, column, max_records=none, fail=True, default='blank') -%}
 
-    {%- call statement('get_column_values', fetch_result=True) %}
+{#-- Prevent querying of db in parsing mode. This works because this macro does not create any new refs. #}
+    {%- if not execute -%}
+        {{ return('') }}
+    {% endif %}
+{#--  #}
 
-        select
-            {{ column }} as value
+    {%- set table_name = table -%}
 
-        from {{ table }}
-        group by 1
-        order by count(*) desc
+    {%- set table_value = adapter.get_relation(database=table_name.database,
+                                          schema=table_name.schema,
+                                         identifier=table_name.identifier) -%}
 
-        {% if max_records is not none %}
-        limit {{ max_records }}
+    {%- call statement('get_column_values', fetch_result=true) %}
+
+        {%- if not table_value and fail -%}
+
+          {{ log("Table doesn't exist..", info=True) }}
+          {{ exceptions.raise_compiler_error("Table to pull columns from doesn't exist. Try running " ~ table ~ " and its dependencies first.") }}
+
+        {%- elif not table_value and not fail -%}
+
+          {{ log("Table doesn't exist..", info=True) }}
+          {{ log("Trying to not fail", info=True) }}
+
+          select '{{default}}' as value
+
+        {%- else -%}
+
+            select
+                {{ column }} as value
+
+            from {{ table }}
+            group by 1
+            order by count(*) desc
+
+            {% if max_records is not none %}
+            limit {{ max_records }}
+            {% endif %}
+
         {% endif %}
 
     {%- endcall -%}
@@ -34,7 +61,7 @@ Returns:
         {%- set values = value_list['data'] | map(attribute=0) | list %}
         {{ return(values) }}
     {%- else -%}
-        {{ return([]) }}
+        {{ return(['']) }}
     {%- endif -%}
 
 {%- endmacro %}

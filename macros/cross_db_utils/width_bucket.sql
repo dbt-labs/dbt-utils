143e6a243fa4b38da082fbf69166ea029a91acc7
Claus Herther <claus@calogica.com>
2019-06-02 09:21:21 -0700

    First stab at adding width_bucket macro

---
+++ macros/cross_db_utils/width_bucket.sql
@@ -0,0 +1,24 @@
+{% macro width_bucket(expr, min_value, max_value, num_buckets) %}
+  {{ adapter_macro('dbt_utils.width_bucket', expr, min_value, max_value, num_buckets) }}
+{% endmacro %}
+
+
+{% macro default__width_bucket(expr, min_value, max_value, num_buckets) -%}
+
+    {%- set bucket_width = '((' ~ max_value ~ ' - ' ~ min_value ~ ')/' ~ num_buckets ~ ')' -%}
+    (
+        case
+            when mod({{ expr }}, {{ bucket_width }}) = 0
+            then 1
+            else 0
+        end
+    ) +
+        ceil(
+            ({{ expr }} - {{ min_value }})/{{ bucket_width }}
+        )
+
+{%- endmacro %}
+
+{% macro snowflake__width_bucket(expr, min_value, max_value, num_buckets) %}
+    width_bucket({{ expr}}, {{ min_value }}, {{ max_value }}, {{ num_buckets }} )
+{% endmacro %}

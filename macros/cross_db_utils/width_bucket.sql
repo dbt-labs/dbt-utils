032ae62bb072310c213ff7141288e4f3dbb8fc12
Claus Herther <claus@calogica.com>
2019-06-02 09:55:12 -0700

    Adds check to make sure values over max go in default bucket

--- macros/cross_db_utils/width_bucket.sql
+++ macros/cross_db_utils/width_bucket.sql
@@ -7,16 +7,20 @@
 
     {%- set bucket_width = '((' ~ max_value ~ ' - ' ~ min_value ~ ')/' ~ num_buckets ~ ')' -%}
     (
+        -- to break ties when the amount is eaxtly at the bucket egde
         case
             when mod({{ expr }}, {{ bucket_width }}) = 0
             then 1
             else 0
         end
     ) +
-        ceil(
-            ({{ expr }} - {{ min_value }})/{{ bucket_width }}
+      -- Anything over max_value goes the N+1 bucket
+        least(
+            ceil(
+                ({{ expr }} - {{ min_value }})/{{ bucket_width }}
+            ),
+            {{ num_buckets }} + 1
         )
-
 {%- endmacro %}
 
 {% macro snowflake__width_bucket(expr, min_value, max_value, num_buckets) %}

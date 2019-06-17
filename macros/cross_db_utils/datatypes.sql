bf938a4ddd7f2f7e5567647ea0544991badcbfd7
Stephen Frazier <siephen@users.noreply.github.com>
2019-06-17 14:15:12 -0500

    Added float and bigint datatype conversions

--- macros/cross_db_utils/datatypes.sql
+++ macros/cross_db_utils/datatypes.sql
@@ -35,3 +35,33 @@
 {% macro snowflake__type_timestamp() %}
     timestamp_ntz
 {% endmacro %}
+
+
+{# float  -------------------------------------------------     #}
+
+{% macro type_float() %}
+  {{ adapter_macro('dbt_utils.type_float') }}
+{% endmacro %}
+
+{% macro default__type_float() %}
+    float
+{% endmacro %}
+
+{% macro bigquery__type_float() %}
+    float64
+{% endmacro %}
+
+
+{# bigint  -------------------------------------------------     #}
+
+{% macro type_bigint() %}
+  {{ adapter_macro('dbt_utils.type_bigint') }}
+{% endmacro %}
+
+{% macro default__type_bigint() %}
+    bigint
+{% endmacro %}
+
+{% macro bigquery__type_bigint() %}
+    int64
+{% endmacro %}

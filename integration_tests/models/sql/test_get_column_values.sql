ed8786bc711129f566f297790928c23cae9e7bc0
emilie <emilie.burke@gmail.com>
2019-07-14 19:51:11 -0400

    revert test to original

--- integration_tests/models/sql/test_get_column_values.sql
+++ integration_tests/models/sql/test_get_column_values.sql
@@ -1,7 +1,6 @@
 
 {% set columns = dbt_utils.get_column_values(ref('data_get_column_values'), 'field') %}
 
-{{ log(target.type, info = True ) }}
 {% if target.type == 'snowflake' %}
 
 select
@@ -16,14 +15,13 @@ select
 from {{ ref('data_get_column_values') }}
 
 {% else %}
-    {{ log(columns, info = True ) }}
-    {{ log("starting select", info = True ) }}
+
 select
-    --{% set columns = columns if columns is iterable else [] %}
+    {% set columns = columns if columns is iterable else [] %}
     {% for column in columns -%}
-    {{ log(column, info = True ) }}
-        --{{dbt_utils.safe_cast("sum(case when field = '" ~ column ~ "' then 1 else 0 end)", dbt_utils.type_string()) }} as count_{{ column }}
-        --{%- if not loop.last %},{% endif -%}
+
+        {{dbt_utils.safe_cast("sum(case when field = '" ~ column ~ "' then 1 else 0 end)", dbt_utils.type_string()) }} as count_{{ column }}
+        {%- if not loop.last %},{% endif -%}
 
     {%- endfor %}
 

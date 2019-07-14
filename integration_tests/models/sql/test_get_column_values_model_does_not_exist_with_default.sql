1c481e4aefbf27f4eec96f37cd60b86fe530b6bf
emilie <emilie.burke@gmail.com>
2019-07-14 19:20:17 -0400

    update default value

--- integration_tests/models/sql/test_get_column_values_model_does_not_exist_with_default.sql
+++ integration_tests/models/sql/test_get_column_values_model_does_not_exist_with_default.sql
@@ -1,8 +1,9 @@
-{% set columns = dbt_utils.get_column_values('data_get_column_values_not_real', 'field', default=[]) %}
+{% set columns = dbt_utils.get_column_values('data_get_column_values_not_real', 'field', default='') %}
 
 {% if target.type == 'snowflake' %}
 
 select
+    {% set columns = columns if columns is iterable else [] %}
     {% for column in columns -%}
 
         sum(case when field = '{{ column }}' then 1 else 0 end) as count_{{ column }}
@@ -15,6 +16,7 @@ from {{ ref('data_get_column_values') }}
 {% else %}
 
 select
+    {% set columns = columns if columns is iterable else [] %}
     {% for column in columns -%}
 
         {{dbt_utils.safe_cast("sum(case when field = '" ~ column ~ "' then 1 else 0 end)", dbt_utils.type_string()) }} as count_{{ column }}

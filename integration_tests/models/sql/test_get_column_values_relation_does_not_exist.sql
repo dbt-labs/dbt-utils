9e77fd87766ed8a79d7abfb71c5716832201b846
emilie <emilie.burke@gmail.com>
2019-07-14 20:16:42 -0400

    add test

---
+++ integration_tests/models/sql/test_get_column_values_relation_does_not_exist.sql
@@ -0,0 +1,29 @@
+{% set columns = dbt_utils.get_column_values(ref('sample'), 'sample_column', default='') %}
+
+{% if target.type == 'snowflake' %}
+
+select
+    {% set columns = columns if columns is iterable else [] %}
+    {% for column in columns -%}
+
+        sum(case when field = '{{ column }}' then 1 else 0 end) as count_{{ column }}
+        {%- if not loop.last %},{% endif -%}
+
+    {%- endfor %}
+
+from {{ ref('data_get_column_values') }}
+
+{% else %}
+
+select
+    {% set columns = columns if columns is iterable else [] %}
+    {% for column in columns -%}
+
+        {{dbt_utils.safe_cast("sum(case when field = '" ~ column ~ "' then 1 else 0 end)", dbt_utils.type_string()) }} as count_{{ column }}
+        {%- if not loop.last %},{% endif -%}
+
+    {%- endfor %}
+
+from {{ ref('data_get_column_values') }}
+
+{% endif %}

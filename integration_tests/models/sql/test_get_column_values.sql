6d1dfda43ac3b5c489e2a451c106778c7ea8975d
emilie <emilie.burke@gmail.com>
2019-07-23 15:42:08 -0400

    update other test

--- integration_tests/models/sql/test_get_column_values.sql
+++ integration_tests/models/sql/test_get_column_values.sql
@@ -1,11 +1,10 @@
 
-{% set columns = dbt_utils.get_column_values(ref('data_get_column_values'), 'field', default = ['']) %}
+{% set columns = dbt_utils.get_column_values(ref('data_get_column_values'), 'field', default = []) %}
 
 
 {% if target.type == 'snowflake' %}
 
 select
-    {% set columns = columns if columns is iterable else [] %}
     {% for column in columns -%}
 
         sum(case when field = '{{ column }}' then 1 else 0 end) as count_{{ column }}
@@ -18,7 +17,6 @@ from {{ ref('data_get_column_values') }}
 {% else %}
 
 select
-    {% set columns = columns if columns is iterable else [] %}
     {% for column in columns -%}
 
         {{dbt_utils.safe_cast("sum(case when field = '" ~ column ~ "' then 1 else 0 end)", dbt_utils.type_string()) }} as count_{{ column }}

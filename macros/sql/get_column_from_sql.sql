d6cd2cb0c39a4eeef7514c0bb107109f0a51d219
Chris Lawliss <chrislawliss@gmail.com>
2019-09-13 11:13:46 -0400

    remove get_column_from_sql

--- macros/sql/get_column_from_sql.sql
+++
@@ -1,9 +0,0 @@
-{% macro get_column_from_sql(sql, column_name) %}
-
-{% if execute %}
-    {{ return(dbt_utils.fetch(sql)[column_name]) }}
-{% else %}
-    {{return([])}}
-{% endif %}
-
-{% endmacro %}

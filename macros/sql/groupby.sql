8100ca40e4b7d4858ad86e683836651661086181
Leon Tchikindas <ltchikindas@gmail.com>
2019-11-07 11:04:37 -0800

    Reducing whitespace in group_by
    reducing whitespace in compiled SQL for legibility

--- macros/sql/groupby.sql
+++ macros/sql/groupby.sql
@@ -1,9 +1,8 @@
-{% macro group_by(n) %}
+{%- macro group_by(n) -%}
 
   group by
-    {% for i in range(1, n + 1) %}
-      {{ i }}
-      {% if not loop.last %} , {% endif %}
-   {% endfor %}
+    {% for i in range(1, n + 1) -%}
+      {{ i }}{{ ',' if not loop.last }}   
+   {%- endfor -%}
 
-{% endmacro %}
+{%- endmacro -%}

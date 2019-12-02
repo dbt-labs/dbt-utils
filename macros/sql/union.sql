a20b13186123cd25fc0018cfd37fd36bf544cb33
Claire Carroll <carroll.claire.e@gmail.com>
2019-12-02 14:14:27 -0500

    Add include param to union.

--- macros/sql/union.sql
+++ macros/sql/union.sql
@@ -1,42 +1,52 @@
-{% macro union_tables(tables, column_override=none, exclude=none, source_column_name=none) -%}
+{% macro union_tables(tables, column_override=none, include=[], exclude=[], source_column_name=none) -%}
+
+    {%- if exclude and include -%}
+        {{ exceptions.raise_compiler_error("Both an exclude and include list were provided to the `union` macro. Only one is allowed") }}
+    {%- endif -%}
 
     {#-- Prevent querying of db in parsing mode. This works because this macro does not create any new refs. #}
     {%- if not execute -%}
         {{ return('') }}
     {% endif %}
 
-    {%- set exclude = exclude if exclude is not none else [] %}
     {%- set column_override = column_override if column_override is not none else {} %}
-    {%- set source_column_name = source_column_name if source_column_name is not none else '_dbt_source_table' %}
+    {%- set source_column_name = source_column_name if source_column_name is not none else '_dbt_source_relation' %}
 
     {%- set table_columns = {} %}
     {%- set column_superset = {} %}
 
     {%- for table in tables -%}
 
-        {%- set _ = table_columns.update({table: []}) %}
+        {%- do table_columns.update({table: []}) %}
 
         {%- do dbt_utils._is_relation(table, 'union_tables') -%}
         {%- set cols = adapter.get_columns_in_relation(table) %}
         {%- for col in cols -%}
 
-        {%- if col.column not in exclude %}
+        {#- If an exclude list was provided and the column is in the list, do nothing #}
+        {%- if exclude and col.column in exclude %}
+
+        {#- If an include list was procided and the column is not in the list, do nothing -#}
+        {%- elif include and col.column not in include %}
+
+        {#- Otherwise add the column to the column superset #}
+        {% else %}
 
             {# update the list of columns in this table #}
-            {%- set _ = table_columns[table].append(col.column) %}
+            {%- do table_columns[table].append(col.column) %}
 
             {%- if col.column in column_superset -%}
 
                 {%- set stored = column_superset[col.column] %}
                 {%- if col.is_string() and stored.is_string() and col.string_size() > stored.string_size() -%}
 
-                    {%- set _ = column_superset.update({col.column: col}) %}
+                    {%- do column_superset.update({col.column: col}) %}
 
                 {%- endif %}
 
             {%- else -%}
 
-                {%- set _ =  column_superset.update({col.column: col}) %}
+                {%- do column_superset.update({col.column: col}) %}
 
             {%- endif -%}
 

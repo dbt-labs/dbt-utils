96fcce7becf2cd8936ecef448a7fbd0cc41524f6
Claire Carroll <carroll.claire.e@gmail.com>
2019-12-03 10:01:19 -0500

    Respect user-provided source_column_name parameter

--- macros/sql/union.sql
+++ macros/sql/union.sql
@@ -81,13 +81,12 @@
 
 {%- endmacro %}
 
-{% macro union_tables(tables, column_override=none, include=[], exclude=[], source_column_name=none) -%}
+{% macro union_tables(tables, column_override=none, include=[], exclude=[], source_column_name='_dbt_source_table') -%}
 
     {% if execute %}
         {{ log("Warning: the `union_tables` macro is no longer supported and will be deprecated in a future release of dbt-utils. Use the `union_relations` macro instead", info=True) }}
     {% endif %}
 
-
-    {{ return(dbt_utils.union_relations(tables, column_override, include, exclude, source_column_name='_dbt_source_table')) }}
+    {{ return(dbt_utils.union_relations(tables, column_override, include, exclude, source_column_name)) }}
 
 {% endmacro %}

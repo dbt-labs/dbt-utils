e3a7de8defd497d74fea260c5962684aadebf84f
Claire Carroll <carroll.claire.e@gmail.com>
2019-12-06 15:11:53 -0500

    Clean up union tests

--- integration_tests/models/sql/test_get_relations_by_prefix_and_union.sql
+++ integration_tests/models/sql/test_get_relations_by_prefix_and_union.sql
@@ -1,14 +1,4 @@
 {{ config(materialized = 'table') }}
 
-
-{% if target.type == 'snowflake' %}
-
-    {% set relations = dbt_utils.get_relations_by_prefix((target.schema | upper), 'data_events_') %}
-    {{ dbt_utils.union_relations(relations) }}
-
-{% else %}
-
-    {% set relations = dbt_utils.get_relations_by_prefix(target.schema, 'data_events_') %}
-    {{ dbt_utils.union_relations(relations) }}
-
-{% endif %}
+{% set relations = dbt_utils.get_relations_by_prefix(target.schema, 'data_events_') %}
+{{ dbt_utils.union_relations(relations) }}

25761d11edd6412a1cf0ac79c7dc4fd4ec134e37
Mjumbe Poe <mjumbe.poe@betterment.com>
2020-01-28 18:51:18 -0500

    Add datediff implementation for postgres

--- integration_tests/models/cross_db_utils/test_datediff.sql
+++ integration_tests/models/cross_db_utils/test_datediff.sql
@@ -6,19 +6,37 @@ with data as (
 )
 
 select
-    -- not implemented for postgres
-    {% if target.type == 'postgres' %}
-        null::text as actual,
-        null::text as expected
-    {% else %}
-        case
-            when datepart = 'hour' then {{ dbt_utils.datediff('first_date', 'second_date', 'hour') }}
-            when datepart = 'day' then {{ dbt_utils.datediff('first_date', 'second_date', 'day') }}
-            when datepart = 'month' then {{ dbt_utils.datediff('first_date', 'second_date', 'month') }}
-            when datepart = 'year' then {{ dbt_utils.datediff('first_date', 'second_date', 'year') }}
-            else null
-        end as actual,
-        result as expected
-    {% endif %}
+
+    case
+        when datepart = 'second' then {{ dbt_utils.datediff('first_date', 'second_date', 'second') }}
+        when datepart = 'minute' then {{ dbt_utils.datediff('first_date', 'second_date', 'minute') }}
+        when datepart = 'hour' then {{ dbt_utils.datediff('first_date', 'second_date', 'hour') }}
+        when datepart = 'day' then {{ dbt_utils.datediff('first_date', 'second_date', 'day') }}
+        when datepart = 'week' then {{ dbt_utils.datediff('first_date', 'second_date', 'week') }}
+        when datepart = 'month' then {{ dbt_utils.datediff('first_date', 'second_date', 'month') }}
+        when datepart = 'year' then {{ dbt_utils.datediff('first_date', 'second_date', 'year') }}
+        when datepart = 'decade' then {{ dbt_utils.datediff('first_date', 'second_date', 'decade') }}
+        when datepart = 'century' then {{ dbt_utils.datediff('first_date', 'second_date', 'century') }}
+        when datepart = 'millennium' then {{ dbt_utils.datediff('first_date', 'second_date', 'millennium') }}
+        else null
+    end as actual,
+    result as expected
 
 from data
+
+-- Also test correct casting of literal values.
+
+union all select {{ dbt_utils.datediff("'1999-12-31 23:59:59.999999'", "'2000-01-01 00:00:00.000000'", "microsecond") }} as actual, 1 as expected
+union all select {{ dbt_utils.datediff("'1999-12-31 23:59:59.999999'", "'2000-01-01 00:00:00.000000'", "millisecond") }} as actual, 1 as expected
+union all select {{ dbt_utils.datediff("'1999-12-31 23:59:59.999999'", "'2000-01-01 00:00:00.000000'", "second") }} as actual, 1 as expected
+union all select {{ dbt_utils.datediff("'1999-12-31 23:59:59.999999'", "'2000-01-01 00:00:00.000000'", "minute") }} as actual, 1 as expected
+union all select {{ dbt_utils.datediff("'1999-12-31 23:59:59.999999'", "'2000-01-01 00:00:00.000000'", "hour") }} as actual, 1 as expected
+union all select {{ dbt_utils.datediff("'1999-12-31 23:59:59.999999'", "'2000-01-01 00:00:00.000000'", "day") }} as actual, 1 as expected
+union all select {{ dbt_utils.datediff("'1999-12-31 23:59:59.999999'", "'2000-01-01 00:00:00.000000'", "week") }} as actual, 0 as expected
+union all select {{ dbt_utils.datediff("'1999-12-31 23:59:59.999999'", "'2000-01-03 00:00:00.000000'", "week") }} as actual, 1 as expected
+union all select {{ dbt_utils.datediff("'1999-12-31 23:59:59.999999'", "'2000-01-01 00:00:00.000000'", "month") }} as actual, 1 as expected
+union all select {{ dbt_utils.datediff("'1999-12-31 23:59:59.999999'", "'2000-01-01 00:00:00.000000'", "quarter") }} as actual, 1 as expected
+union all select {{ dbt_utils.datediff("'1999-12-31 23:59:59.999999'", "'2000-01-01 00:00:00.000000'", "year") }} as actual, 1 as expected
+union all select {{ dbt_utils.datediff("'1999-12-31 23:59:59.999999'", "'2000-01-01 00:00:00.000000'", "decade") }} as actual, 1 as expected
+union all select {{ dbt_utils.datediff("'1999-12-31 23:59:59.999999'", "'2001-01-01 00:00:00.000000'", "century") }} as actual, 1 as expected
+union all select {{ dbt_utils.datediff("'1999-12-31 23:59:59.999999'", "'2001-01-01 00:00:00.000000'", "millennium") }} as actual, 1 as expected

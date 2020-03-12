0ef323a6bc5b97b01765933985a51fd23b973ebd
Charlie Briggs <charlie.briggs@signal-ai.com>
2020-03-12 16:41:09 +0000

    Remove extraneous whitespace from concat/current_timestamp/date_trunc/surrogate_key macros

--- macros/cross_db_utils/date_trunc.sql
+++ macros/cross_db_utils/date_trunc.sql
@@ -1,6 +1,6 @@
-{% macro date_trunc(datepart, date) %}
+{% macro date_trunc(datepart, date) -%}
   {{ adapter_macro('dbt_utils.date_trunc', datepart, date) }}
-{% endmacro %}
+{%- endmacro %}
 
 {% macro default__date_trunc(datepart, date) %}
     date_trunc('{{datepart}}', {{date}})
@@ -8,9 +8,8 @@
 
 {% macro bigquery__date_trunc(datepart, date) %}
     timestamp_trunc(
-        cast({{date}} as timestamp), 
+        cast({{date}} as timestamp),
         {{datepart}}
     )
-    
 
 {% endmacro %}
--- macros/sql/surrogate_key.sql
+++ macros/sql/surrogate_key.sql
@@ -1,16 +1,16 @@
 {%- macro surrogate_key() -%}
 
-{% set fields = [] %}
+{%- set fields = [] -%}
 
 {%- for field in varargs -%}
 
-    {% set _ = fields.append(
+    {%- set _ = fields.append(
         "coalesce(cast(" ~ field ~ " as " ~ dbt_utils.type_string() ~ "), '')"
-    ) %}
+    ) -%}
 
-    {% if not loop.last %}
-        {% set _ = fields.append("'-'") %}
-    {% endif %}
+    {%- if not loop.last %}
+        {%- set _ = fields.append("'-'") -%}
+    {%- endif -%}
 
 {%- endfor -%}
 

9afa67a28bc19ebb545fc927525f210020707253
Elliott O'Hara <elliott.ohara@getupside.com>
2020-06-02 11:24:31 -0500

    Add test for json path not null

---
+++ integration_tests/models/schema_tests/test_json_paths_not_null.sql
@@ -0,0 +1,14 @@
+with source as (
+    select * from {{ ref('data_test_json_paths_not_null') }}
+), sanitized as (
+select
+    id,
+    parse_json(CONCAT('{\'settings\': {\'emailAddress\':\'',
+            email_address,
+            '\', \'displayName\':\'',
+            display_name,
+            '\'}}')) as json_field
+    from
+source
+)
+select * from sanitized

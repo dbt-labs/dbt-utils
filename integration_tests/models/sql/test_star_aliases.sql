171970e9ef19a9654701b078db257ac9892d4ff6
Elliott O'Hara <elliott.ohara@getupside.com>
2020-06-10 13:56:43 -0500

    Allow aliasing individual columns from star macro.

---
+++ integration_tests/models/sql/test_star_aliases.sql
@@ -0,0 +1,16 @@
+
+-- TODO : Should the star macro use a case-insensitive comparison for the `except` field on Snowflake?
+
+{% set aliases = {'FIELD_3':'aliased'} if target.type == 'snowflake' else {'field_3':'aliased'} %}
+
+
+with data as (
+
+    select
+        {{ dbt_utils.star(from=ref('data_star'), aliases=aliases) }}
+
+    from {{ ref('data_star') }}
+
+)
+
+select * from data

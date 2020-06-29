58c0818b0f665199b8da2b5fa04bf5d01c06b31b
Claire Carroll <carroll.claire.e@gmail.com>
2020-06-29 10:50:31 -0400

    Strip leading prefix [ci skip]

--- macros/cross_db_utils/_is_ephemeral.sql
+++ macros/cross_db_utils/_is_ephemeral.sql
@@ -1,9 +1,14 @@
 {% macro _is_ephemeral(obj, macro) %}
     {%- if obj.is_cte -%}
+        {% if obj.name.startswith('__dbt__CTE__') %}
+            {% set model_name = obj.name[12:] %}
+        {% else %}
+            {% set model_name = obj.name %}
+        {%- endif -%}
         {% set error_message %}
 The `{{ macro }}` macro cannot be used with ephemeral models, as it relies on the information schema.
 
-`{{ obj.name }}` is an ephemeral model. Consider making it a view or table instead.
+`{{ model_name }}` is an ephemeral model. Consider making it a view or table instead.
         {% endset %}
         {%- do exceptions.raise_compiler_error(error_message) -%}
     {%- endif -%}

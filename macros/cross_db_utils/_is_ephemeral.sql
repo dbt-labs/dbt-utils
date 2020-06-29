0e4eabffea35200189b023945bf4416184b3af7f
Claire Carroll <carroll.claire.e@gmail.com>
2020-06-29 10:21:53 -0400

    Fix typo [ci skip]

--- macros/cross_db_utils/_is_ephemeral.sql
+++ macros/cross_db_utils/_is_ephemeral.sql
@@ -3,7 +3,7 @@
         {% set error_message %}
 The `{{ macro }}` macro cannot be used with ephemeral models, as it relies on the information schema.
 
-`{{ obj.name }}` is an ephemeral model. Consider making is a view or table instead.
+`{{ obj.name }}` is an ephemeral model. Consider making it a view or table instead.
         {% endset %}
         {%- do exceptions.raise_compiler_error(error_message) -%}
     {%- endif -%}

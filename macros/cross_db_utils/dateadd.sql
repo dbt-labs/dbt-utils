fecd4a40db7625a5cc29a7d2ab36863c29a9fdb2
Jeremy Cohen <jeremy@fishtownanalytics.com>
2020-08-20 12:13:11 -0400

    Lost & found: pg dateadd, datediff

--- macros/cross_db_utils/dateadd.sql
+++ macros/cross_db_utils/dateadd.sql
@@ -22,3 +22,9 @@
         )
 
 {% endmacro %}
+
+{% macro postgres__dateadd(datepart, interval, from_date_or_timestamp) %}
+
+    {{ from_date_or_timestamp }} + ((interval '1 {{ datepart }}') * ({{ interval }}))
+
+{% endmacro %}

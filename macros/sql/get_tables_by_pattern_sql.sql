8ee78fc5d8b3f20c82a80995da8af019cc896cc1
Tom ONeill <tom.oneill@m-kopa.com>
2020-11-05 19:02:10 +0000

    Fix tables_by_pattern

--- macros/sql/get_tables_by_pattern_sql.sql
+++ macros/sql/get_tables_by_pattern_sql.sql
@@ -15,6 +15,18 @@
 {% endmacro %}
 
 
+{% macro sqlserver__get_tables_by_pattern_sql(schema_pattern, table_pattern, exclude='', database=target.database) %}
+
+        SELECT DISTINCT
+            table_schema AS "table_schema",
+            table_name AS "table_name"
+        FROM {{database}}.information_schema.tables
+        WHERE table_schema LIKE '{{ schema_pattern }}'
+        AND table_name LIKE '{{ table_pattern }}'
+        AND table_name NOT LIKE '{{ exclude }}'
+
+{% endmacro %}
+
 {% macro bigquery__get_tables_by_pattern_sql(schema_pattern, table_pattern, exclude='', database=target.database) %}
 
     {% if '%' in schema_pattern %}

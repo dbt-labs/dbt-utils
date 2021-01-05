39775c64513d5b6695edbf61fe2875c772a72ea0
Anders Swanson <swanson.anders@gmail.com>
2021-01-05 11:55:21 -0800

    sweet hail mary nothings

--- macros/sql/safe_add.sql
+++ macros/sql/safe_add.sql
@@ -1,6 +1,7 @@
 {%- macro safe_add() -%}
-    {# needed for safe_add to allow for non-keyword arguments #}
-    {% set dummy_var = args %}
+    {# needed for safe_add to allow for non-keyword arguments see SO post #}
+    {# https://stackoverflow.com/questions/13944751/args-kwargs-in-jinja2-macros #}
+    {# {{args}} #}
     {{ return(adapter.dispatch('safe_add', packages = dbt_utils._get_utils_namespaces())(*args)) }}
 {% endmacro %}
 

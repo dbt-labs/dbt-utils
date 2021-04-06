e81cd737a65ce21290f5ff7a522c1d0bd4724af7
Chad Upjohn <chad.upjohn@stratasan.com>
2021-04-06 05:12:32 -0500

    adding handling for snowflake external table in get_tables_by_pattern

--- macros/sql/get_tables_by_pattern_sql.sql
+++ macros/sql/get_tables_by_pattern_sql.sql
@@ -10,6 +10,7 @@
             table_name as "table_name",
             case table_type
                 when 'BASE TABLE' then 'table'
+                when 'EXTERNAL TABLE' then 'external'
                 else lower(table_type)
             end as "table_type"
         from {{ database }}.information_schema.tables

7d745e6b8b068a01d55fc094210614221acdc0e8
Yu ISHIKAWA <yuu.ishikawa@gmail.com>
2021-08-17 10:24:39 +0900

    Fix the model and tests

--- integration_tests/models/schema_tests/test_sequential_values.sql
+++ integration_tests/models/schema_tests/test_sequential_values.sql
@@ -2,14 +2,14 @@
 select
     i AS id,
     -- interval = 1
-    date '2021-01-01' + make_interval(hours => i) AS hour_interval,
-    date '2021-01-01' + make_interval(days => i) AS day_interval,
-    date '2021-01-01' + make_interval(months => i) AS month_interval,
-    date '2021-01-01' + make_interval(years => i) AS year_interval,
+    date '2021-01-01' + make_interval(hours => i) AS one_hour_interval,
+    date '2021-01-01' + make_interval(days => i) AS one_day_interval,
+    date '2021-01-01' + make_interval(months => i) AS one_month_interval,
+    date '2021-01-01' + make_interval(years => i) AS one_year_interval,
     -- interval = 2
     date '2021-01-01' + make_interval(hours => i * 2) AS two_hours_interval,
     date '2021-01-01' + make_interval(days => i * 2) AS two_days_interval,
-    date '2021-01-01' + make_interval(months => i * 2) AS two_month_interval,
+    date '2021-01-01' + make_interval(months => i * 2) AS two_months_interval,
     date '2021-01-01' + make_interval(years => i * 2) AS two_years_interval,
     case
         when i >= 5 then i

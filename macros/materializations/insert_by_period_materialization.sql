3493d3f30c2f4f92ec8457c0f981128e367db806
Daniel Horvath <hdaniel@secretsaucepartners.com>
2021-08-17 10:16:08 +0200

    Update legacy macro to 0.21.latest incremental

--- macros/materializations/insert_by_period_materialization.sql
+++ macros/materializations/insert_by_period_materialization.sql
@@ -1,189 +1,89 @@
-{% macro get_period_boundaries(target_schema, target_table, timestamp_field, start_date, stop_date, period) -%}
-    {{ return(adapter.dispatch('get_period_boundaries', 'dbt_utils')(target_schema, target_table, timestamp_field, start_date, stop_date, period)) }}
-{% endmacro %}
-
-{% macro default__get_period_boundaries(target_schema, target_table, timestamp_field, start_date, stop_date, period) -%}
-
-  {% call statement('period_boundaries', fetch_result=True) -%}
-    with data as (
-      select
-          coalesce(max("{{timestamp_field}}"), '{{start_date}}')::timestamp as start_timestamp,
-          coalesce(
-            {{dbt_utils.dateadd('millisecond',
-                                -1,
-                                "nullif('" ~ stop_date ~ "','')::timestamp")}},
-            {{dbt_utils.current_timestamp()}}
-          ) as stop_timestamp
-      from "{{target_schema}}"."{{target_table}}"
-    )
-
-    select
-      start_timestamp,
-      stop_timestamp,
-      {{dbt_utils.datediff('start_timestamp',
-                           'stop_timestamp',
-                           period)}}  + 1 as num_periods
-    from data
-  {%- endcall %}
-
-{%- endmacro %}
-
-{% macro get_period_sql(target_cols_csv, sql, timestamp_field, period, start_timestamp, stop_timestamp, offset) -%}
-    {{ return(adapter.dispatch('get_period_sql', 'dbt_utils')(target_cols_csv, sql, timestamp_field, period, start_timestamp, stop_timestamp, offset)) }}
-{% endmacro %}
-
-{% macro default__get_period_sql(target_cols_csv, sql, timestamp_field, period, start_timestamp, stop_timestamp, offset) -%}
-
-  {%- set period_filter -%}
-    ("{{timestamp_field}}" >  '{{start_timestamp}}'::timestamp + interval '{{offset}} {{period}}' and
-     "{{timestamp_field}}" <= '{{start_timestamp}}'::timestamp + interval '{{offset}} {{period}}' + interval '1 {{period}}' and
-     "{{timestamp_field}}" <  '{{stop_timestamp}}'::timestamp)
-  {%- endset -%}
-
-  {%- set filtered_sql = sql | replace("__PERIOD_FILTER__", period_filter) -%}
-
-  select
-    {{target_cols_csv}}
-  from (
-    {{filtered_sql}}
-  )
-
-{%- endmacro %}
 
 {% materialization insert_by_period, default -%}
-  {%- set timestamp_field = config.require('timestamp_field') -%}
-  {%- set start_date = config.require('start_date') -%}
-  {%- set stop_date = config.get('stop_date') or '' -%}}
-  {%- set period = config.get('period') or 'week' -%}
 
-  {%- if sql.find('__PERIOD_FILTER__') == -1 -%}
-    {%- set error_message -%}
-      Model '{{ model.unique_id }}' does not include the required string '__PERIOD_FILTER__' in its sql
-    {%- endset -%}
-    {{ exceptions.raise_compiler_error(error_message) }}
-  {%- endif -%}
+  {% set unique_key = config.get('unique_key') %}
 
-  {%- set identifier = model['name'] -%}
+  {% set target_relation = this.incorporate(type='table') %}
+  {% set existing_relation = load_relation(this) %}
+  {% set tmp_relation = make_temp_relation(target_relation) %}
+  {%- set full_refresh_mode = (should_full_refresh()) -%}
 
-  {%- set old_relation = adapter.get_relation(database=database, schema=schema, identifier=identifier) -%}
-  {%- set target_relation = api.Relation.create(identifier=identifier, schema=schema, type='table') -%}
+  {% set on_schema_change = incremental_validate_on_schema_change(config.get('on_schema_change'), default='ignore') %}
 
-  {%- set non_destructive_mode = (flags.NON_DESTRUCTIVE == True) -%}
-  {%- set full_refresh_mode = (flags.FULL_REFRESH == True) -%}
+  {% set tmp_identifier = model['name'] + '__dbt_tmp' %}
+  {% set backup_identifier = model['name'] + "__dbt_backup" %}
 
-  {%- set exists_as_table = (old_relation is not none and old_relation.is_table) -%}
-  {%- set exists_not_as_table = (old_relation is not none and not old_relation.is_table) -%}
+  -- the intermediate_ and backup_ relations should not already exist in the database; get_relation
+  -- will return None in that case. Otherwise, we get a relation that we can drop
+  -- later, before we try to use this name for the current operation. This has to happen before
+  -- BEGIN, in a separate transaction
+  {% set preexisting_intermediate_relation = adapter.get_relation(identifier=tmp_identifier, 
+                                                                  schema=schema,
+                                                                  database=database) %}                                               
+  {% set preexisting_backup_relation = adapter.get_relation(identifier=backup_identifier,
+                                                            schema=schema,
+                                                            database=database) %}
+  {{ drop_relation_if_exists(preexisting_intermediate_relation) }}
+  {{ drop_relation_if_exists(preexisting_backup_relation) }}
 
-  {%- set should_truncate = (non_destructive_mode and full_refresh_mode and exists_as_table) -%}
-  {%- set should_drop = (not should_truncate and (full_refresh_mode or exists_not_as_table)) -%}
-  {%- set force_create = (flags.FULL_REFRESH and not flags.NON_DESTRUCTIVE) -%}
+  {{ run_hooks(pre_hooks, inside_transaction=False) }}
 
-  -- setup
-  {% if old_relation is none -%}
-    -- noop
-  {%- elif should_truncate -%}
-    {{adapter.truncate_relation(old_relation)}}
-  {%- elif should_drop -%}
-    {{adapter.drop_relation(old_relation)}}
-    {%- set old_relation = none -%}
-  {%- endif %}
+  -- `BEGIN` happens here:
+  {{ run_hooks(pre_hooks, inside_transaction=True) }}
 
-  {{run_hooks(pre_hooks, inside_transaction=False)}}
+  {% set to_drop = [] %}
 
-  -- `begin` happens here, so `commit` after it to finish the transaction
-  {{run_hooks(pre_hooks, inside_transaction=True)}}
-  {% call statement() -%}
-    begin; -- make extra sure we've closed out the transaction
-    commit;
-  {%- endcall %}
+  {# -- first check whether we want to full refresh for source view or config reasons #}
+  {% set trigger_full_refresh = (full_refresh_mode or existing_relation.is_view) %}
 
-  -- build model
-  {% if force_create or old_relation is none -%}
-    {# Create an empty target table -#}
-    {% call statement('main') -%}
-      {%- set empty_sql = sql | replace("__PERIOD_FILTER__", 'false') -%}
-      {{create_table_as(False, target_relation, empty_sql)}};
-    {%- endcall %}
-  {%- endif %}
+  {% if existing_relation is none %}
+      {% set build_sql = create_table_as(False, target_relation, sql) %}
+{% elif trigger_full_refresh %}
+      {#-- Make sure the backup doesn't exist so we don't encounter issues with the rename below #}
+      {% set tmp_identifier = model['name'] + '__dbt_tmp' %}
+      {% set backup_identifier = model['name'] + '__dbt_backup' %}
+      {% set intermediate_relation = existing_relation.incorporate(path={"identifier": tmp_identifier}) %}
+      {% set backup_relation = existing_relation.incorporate(path={"identifier": backup_identifier}) %}
 
-  {% set _ = dbt_utils.get_period_boundaries(schema,
-                                              identifier,
-                                              timestamp_field,
-                                              start_date,
-                                              stop_date,
-                                              period) %}
-  {%- set start_timestamp = load_result('period_boundaries')['data'][0][0] | string -%}
-  {%- set stop_timestamp = load_result('period_boundaries')['data'][0][1] | string -%}
-  {%- set num_periods = load_result('period_boundaries')['data'][0][2] | int -%}
+      {% set build_sql = create_table_as(False, intermediate_relation, sql) %}
+      {% set need_swap = true %}
+      {% do to_drop.append(backup_relation) %}
+  {% else %}
+    {% do run_query(create_table_as(True, tmp_relation, sql)) %}
+    {% do adapter.expand_target_column_types(
+             from_relation=tmp_relation,
+             to_relation=target_relation) %}
+    {% do process_schema_changes(on_schema_change, tmp_relation, existing_relation) %}
+    {% set build_sql = incremental_upsert(tmp_relation, target_relation, unique_key=unique_key) %}
+  
+  {% endif %}
 
-  {% set target_columns = adapter.get_columns_in_relation(target_relation) %}
-  {%- set target_cols_csv = target_columns | map(attribute='quoted') | join(', ') -%}
-  {%- set loop_vars = {'sum_rows_inserted': 0} -%}
+  {% call statement("main") %}
+      {{ build_sql }}
+  {% endcall %}
 
-  -- commit each period as a separate transaction
-  {% for i in range(num_periods) -%}
-    {%- set msg = "Running for " ~ period ~ " " ~ (i + 1) ~ " of " ~ (num_periods) -%}
-    {{ dbt_utils.log_info(msg) }}
+  {% if need_swap %} 
+      {% do adapter.rename_relation(target_relation, backup_relation) %} 
+      {% do adapter.rename_relation(intermediate_relation, target_relation) %} 
+  {% endif %}
 
-    {%- set tmp_identifier = model['name'] ~ '__dbt_incremental_period' ~ i ~ '_tmp' -%}
-    {%- set tmp_relation = api.Relation.create(identifier=tmp_identifier,
-                                               schema=schema, type='table') -%}
-    {% call statement() -%}
-      {% set tmp_table_sql = dbt_utils.get_period_sql(target_cols_csv,
-                                                       sql,
-                                                       timestamp_field,
-                                                       period,
-                                                       start_timestamp,
-                                                       stop_timestamp,
-                                                       i) %}
-      {{dbt.create_table_as(True, tmp_relation, tmp_table_sql)}}
-    {%- endcall %}
+  {% do persist_docs(target_relation, model) %}
 
-    {{adapter.expand_target_column_types(from_relation=tmp_relation,
-                                         to_relation=target_relation)}}
-    {%- set name = 'main-' ~ i -%}
-    {% call statement(name, fetch_result=True) -%}
-      insert into {{target_relation}} ({{target_cols_csv}})
-      (
-          select
-              {{target_cols_csv}}
-          from {{tmp_relation.include(schema=False)}}
-      );
-    {%- endcall %}
-    {% set result = load_result('main-' ~ i) %}
-    {% if 'response' in result.keys() %} {# added in v0.19.0 #}
-        {% set rows_inserted = result['response']['rows_affected'] %}
-    {% else %} {# older versions #}
-        {% set rows_inserted = result['status'].split(" ")[2] | int %}
-    {% endif %}
-    
-    {%- set sum_rows_inserted = loop_vars['sum_rows_inserted'] + rows_inserted -%}
-    {%- if loop_vars.update({'sum_rows_inserted': sum_rows_inserted}) %} {% endif -%}
+  {% if existing_relation is none or existing_relation.is_view or should_full_refresh() %}
+    {% do create_indexes(target_relation) %}
+  {% endif %}
 
-    {%- set msg = "Ran for " ~ period ~ " " ~ (i + 1) ~ " of " ~ (num_periods) ~ "; " ~ rows_inserted ~ " records inserted" -%}
-    {{ dbt_utils.log_info(msg) }}
+  {{ run_hooks(post_hooks, inside_transaction=True) }}
 
-  {%- endfor %}
+  -- `COMMIT` happens here
+  {% do adapter.commit() %}
 
-  {% call statement() -%}
-    begin;
-  {%- endcall %}
+  {% for rel in to_drop %}
+      {% do adapter.drop_relation(rel) %}
+  {% endfor %}
 
-  {{run_hooks(post_hooks, inside_transaction=True)}}
+  {{ run_hooks(post_hooks, inside_transaction=False) }}
 
-  {% call statement() -%}
-    commit;
-  {%- endcall %}
-
-  {{run_hooks(post_hooks, inside_transaction=False)}}
-
-  {%- set status_string = "INSERT " ~ loop_vars['sum_rows_inserted'] -%}
-
-  {% call noop_statement('main', status_string) -%}
-    -- no-op
-  {%- endcall %}
-
-  -- Return the relations created in this materialization
-  {{ return({'relations': [target_relation]}) }}  
+  {{ return({'relations': [target_relation]}) }}
 
 {%- endmaterialization %}

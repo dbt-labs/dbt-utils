d708708afdd4481ef32797f74e6190e0536377ce
Adam Stone <adamstone@gmail.com>
2021-10-31 08:44:25 +0000

    change to regexp and update integration test

--- macros/web/get_url_host.sql
+++ macros/web/get_url_host.sql
@@ -4,25 +4,21 @@
 
 {% macro default__get_url_host(field) -%}
 
-{%- set parsed = 
+{%- set parsed =
     dbt_utils.split_part(
         dbt_utils.split_part(
-            dbt_utils.replace(
-                dbt_utils.replace(
-                    dbt_utils.replace(field, "'android-app://'", "''"
-                    ), "'http://'", "''"
-                ), "'https://'", "''"
+            regexp_replace(field '^[-A-Za-z]+:\/\/', "''"
             ), "'/'", 1
         ), "'?'", 1
     )
-    
+
 -%}
 
-     
+
     {{ dbt_utils.safe_cast(
         parsed,
         dbt_utils.type_string()
         )}}
-        
+
 
 {%- endmacro %}

f5c59665bbb1c862bcb433cf50f86773ba0ea6af
Adam Stone <adam@netlify.com>
2021-11-07 11:06:03 +0000

    Add line breaks back in

--- macros/web/get_url_host.sql
+++ macros/web/get_url_host.sql
@@ -1,7 +1,9 @@
 {% macro get_url_host(field) -%}
     {{ return(adapter.dispatch('get_url_host', 'dbt_utils')(field)) }}
 {% endmacro %}
+
 {% macro default__get_url_host(field) -%}
+
 {%- set parsed =
     dbt_utils.split_part(
         dbt_utils.split_part(
@@ -16,9 +18,11 @@
 
 -%}
 
+
     {{ dbt_utils.safe_cast(
         parsed,
         dbt_utils.type_string()
         )}}
+
 
 {%- endmacro %}

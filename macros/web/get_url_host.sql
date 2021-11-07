067688ab74be4c931bd44773ce3ac334d5bc6463
Adam Stone <adam@netlify.com>
2021-11-07 10:56:14 +0000

    let's try this

--- macros/web/get_url_host.sql
+++ macros/web/get_url_host.sql
@@ -4,12 +4,30 @@
 
 {% macro default__get_url_host(field) -%}
 
-{% set replace_schema = regexp_replace(field, '^[-A-Za-z]+:\/\/', "''") | safe %}
+{% set replace_schema = regexp_replace(field, '^[-A-Za-z]+:\/\/', "''") %}
+
+{% set payment_methods_query %}
+{{ dbt_utils.safe_cast(
+    replace_schema,
+    dbt_utils.type_string()
+    )}}
+{% endset %}
+
+{% set results = run_query(payment_methods_query) %}
+
+{% if execute %}
+{# Return the first column #}
+{% set results_list = results.columns[0].values()[0] %}
+{% else %}
+{% set results_list = [] %}
+{% endif %}
+
+{{ return(results_list) }}
 
 {%- set parsed =
     dbt_utils.split_part(
         dbt_utils.split_part(
-            replace_schema, "'/'", 1
+            results_list, "'/'", 1
             ), "'?'", 1
         )
 -%}

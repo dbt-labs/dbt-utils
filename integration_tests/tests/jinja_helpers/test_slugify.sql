7feff1ed7424fba1bb9f91c931e48842e44b6369
Joel Labes <joel.labes@dbtlabs.com>
2021-11-09 17:53:08 +1300

    Change limit_zero to be a macro

--- integration_tests/tests/jinja_helpers/test_slugify.sql
+++ integration_tests/tests/jinja_helpers/test_slugify.sql
@@ -1,6 +1,6 @@
 {% if dbt_utils.slugify('!Hell0 world-hi') == 'hell0_world_hi' %}
     {# Return 0 rows for the test to pass #}
-    select 1 as col_name {{ limit_zero }}
+    select 1 as col_name {{ limit_zero() }}
 {% else %}
     {# Return >0 rows for the test to fail #}
     select 1

2ed471bb3a1f9f6a748c4235ead5b71d7b910f95
jmriego <pris.t.macodg@gmail.com>
2021-11-11 03:12:50 +0100

    return default in get_column_values if no execute (#386)
    * return default in get_column_values if no execute
    
    * Update CHANGELOG.md
    
    * Update macros/sql/get_column_values.sql
    
    Co-authored-by: Jason Ganz <jason.ganz64@gmail.com>
    
    * Update require-dbt-version
    
    Co-authored-by: Joel Labes <joel.labes@dbtlabs.com>
    Co-authored-by: Jason Ganz <jason.ganz64@gmail.com>

--- macros/sql/get_column_values.sql
+++ macros/sql/get_column_values.sql
@@ -3,10 +3,12 @@
 {% endmacro %}
 
 {% macro default__get_column_values(table, column, order_by='count(*) desc', max_records=none, default=none) -%}
-
+{% if default is none %}
+    {% set default = [] %}
+{% endif %}
     {#-- Prevent querying of db in parsing mode. This works because this macro does not create any new refs. #}
     {%- if not execute -%}
-        {{ return('') }}
+        {{ return(default) }}
     {% endif %}
 
     {%- set target_relation = adapter.get_relation(database=table.database,

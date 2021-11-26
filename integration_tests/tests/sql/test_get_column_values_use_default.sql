e2b13d948026b88aaf7778db064d2bc05f716028
Joel Labes <joel.labes@dbtlabs.com>
2021-11-26 15:25:27 +1300

    Create integration test for a dropped relation

---
+++ integration_tests/tests/sql/test_get_column_values_use_default.sql
@@ -0,0 +1,25 @@
+
+{% set column_values = dbt_utils.get_column_values(ref('data_get_column_values_dropped'), 'field', default=['y', 'z'], order_by="field") %}
+
+with expected as (
+    select { dbt_utils.safe_cast('y', dbt_utils.type_string()) }} as expected union all
+    select { dbt_utils.safe_cast('z', dbt_utils.type_string()) }} as expected 
+),
+
+actual as (
+    select 
+        {% for val in column_values %}
+            {{ dbt_utils.safe_cast(val, dbt_utils.type_string()) }} as 'actual'
+            {% if not loop.last %}
+                union all
+            {% endif %}
+        {% endfor %}
+),
+
+failures as (
+    select * from actual
+    except 
+    select * from expected
+)
+
+select * from failures

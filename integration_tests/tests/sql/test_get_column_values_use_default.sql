08595370b3f078c82043e78da066e30e61f92050
Joel Labes <joel.labes@dbtlabs.com>
2021-12-02 14:17:17 +1300

    Add missing curlies

--- integration_tests/tests/sql/test_get_column_values_use_default.sql
+++ integration_tests/tests/sql/test_get_column_values_use_default.sql
@@ -2,8 +2,8 @@
 {% set column_values = dbt_utils.get_column_values(ref('data_get_column_values_dropped'), 'field', default=['y', 'z'], order_by="field") %}
 
 with expected as (
-    select { dbt_utils.safe_cast('y', dbt_utils.type_string()) }} as expected union all
-    select { dbt_utils.safe_cast('z', dbt_utils.type_string()) }} as expected 
+    select {{ dbt_utils.safe_cast('y', dbt_utils.type_string()) }} as expected union all
+    select {{ dbt_utils.safe_cast('z', dbt_utils.type_string()) }} as expected 
 ),
 
 actual as (

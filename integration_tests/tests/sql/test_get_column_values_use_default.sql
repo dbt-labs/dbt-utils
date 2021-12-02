614a8fc1c102c1f34a40c393fe054e882c8362af
Joel Labes <joel.labes@dbtlabs.com>
2021-12-02 14:44:40 +1300

    bigquery compat (they don't like except)

--- integration_tests/tests/sql/test_get_column_values_use_default.sql
+++ integration_tests/tests/sql/test_get_column_values_use_default.sql
@@ -2,14 +2,14 @@
 {% set column_values = dbt_utils.get_column_values(ref('data_get_column_values_dropped'), 'field', default=['y', 'z'], order_by="field") %}
 
 with expected as (
-    select {{ dbt_utils.safe_cast("'y'", dbt_utils.type_string()) }} as expected union all
-    select {{ dbt_utils.safe_cast("'z'", dbt_utils.type_string()) }} as expected 
+    select {{ dbt_utils.safe_cast("'y'", dbt_utils.type_string()) }} as expected_column_value union all
+    select {{ dbt_utils.safe_cast("'z'", dbt_utils.type_string()) }} as expected_column_value
 ),
 
 actual as (
      
         {% for val in column_values %}
-            select {{ dbt_utils.safe_cast("'" ~ val ~ "'", dbt_utils.type_string()) }} as actual
+            select {{ dbt_utils.safe_cast("'" ~ val ~ "'", dbt_utils.type_string()) }} as actual_column_value
             {% if not loop.last %}
                 union all
             {% endif %}
@@ -18,8 +18,9 @@ actual as (
 
 failures as (
     select * from actual
-    except 
-    select * from expected
+    where actual.actual_column_value not in (
+        select expected.expected_column_value from expected
+    )
 )
 
 select * from failures

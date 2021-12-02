fd8dc17ba9df576fcaef9c16bb0899e528018ba3
Joel Labes <joel.labes@dbtlabs.com>
2021-12-02 14:53:41 +1300

    Backport android url changes from #426 (#452)

--- macros/web/get_url_host.sql
+++ macros/web/get_url_host.sql
@@ -4,23 +4,24 @@
 
 {% macro default__get_url_host(field) -%}
 
-{%- set parsed = 
+{%- set parsed =
     dbt_utils.split_part(
         dbt_utils.split_part(
             dbt_utils.replace(
-                dbt_utils.replace(field, "'http://'", "''"
+                dbt_utils.replace(
+                    dbt_utils.replace(field, "'android-app://'", "''"
+                    ), "'http://'", "''"
                 ), "'https://'", "''"
             ), "'/'", 1
         ), "'?'", 1
     )
-    
+
 -%}
 
-     
+
     {{ dbt_utils.safe_cast(
         parsed,
         dbt_utils.type_string()
         )}}
-        
 
 {%- endmacro %}

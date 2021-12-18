d1cca6893430789d12ef175c15d44b70a52af2b6
robertdefilippi <robert.defilippi@gmail.com>
2021-12-18 12:03:19 -0500

    Update readme

--- macros/sql/star.sql
+++ macros/sql/star.sql
@@ -1,8 +1,8 @@
-{% macro star(from, relation_alias=False, except=[], prefix='', suffix='') -%}
-    {{ return(adapter.dispatch('star', 'dbt_utils')(from, relation_alias, except, prefix, suffix)) }}
+{% macro star(from, relation_alias=False, except=[], regex=False, prefix='', suffix='') -%}
+    {{ return(adapter.dispatch('star', 'dbt_utils')(from, relation_alias, except, regex, prefix, suffix)) }}
 {% endmacro %}
 
-{% macro default__star(from, relation_alias=False, except=[], prefix='', suffix='') -%}
+{% macro default__star(from, relation_alias=False, except=[], regex=False, prefix='', suffix='') -%}
     {%- do dbt_utils._is_relation(from, 'star') -%}
     {%- do dbt_utils._is_ephemeral(from, 'star') -%}
 
@@ -16,16 +16,26 @@
     {%- set except = except | map("lower") | list %}
     {%- for col in cols -%}
 
-        {%- if col.column|lower not in except -%}
+        {%- if col.column | lower not in except -%}
             {% do include_cols.append(col.column) %}
 
         {%- endif %}
     {%- endfor %}
 
-    {%- for col in include_cols %}
+
+    {%- for col in include_cols if not regex %}
 
         {%- if relation_alias %}{{ relation_alias }}.{% else %}{%- endif -%}{{ adapter.quote(col)|trim }} as {{ adapter.quote(prefix ~ col ~ suffix)|trim }}
         {%- if not loop.last %},{{ '\n  ' }}{% endif %}
 
+    {% else %}
+
+        {%- set col = col.column | string -%}
+        {%- if modules.re.match(regex, current_column, modules.re.IGNORECASE) -%}
+            {%- if relation_alias %}{{ relation_alias }}.{% else %}{%- endif -%}{{ adapter.quote(col)|trim }} as {{ adapter.quote(prefix ~ col ~ suffix)|trim }}
+            {%- if not loop.last %},{{ '\n  ' }}{% endif %}
+
+      {%- endif -%}
     {%- endfor -%}
+
 {%- endmacro %}

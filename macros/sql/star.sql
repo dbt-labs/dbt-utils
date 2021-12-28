9030eed2291ca975cedcfe0ba2f3394f750a4be2
Robert Ramos <rramos@clearcover.com>
2021-12-28 15:07:24 -0800

    changed star macro

--- macros/sql/star.sql
+++ macros/sql/star.sql
@@ -1,31 +1,43 @@
-{% macro star(from, relation_alias=False, except=[], prefix='', suffix='') -%}
-    {{ return(adapter.dispatch('star', 'cc_dbt_utils')(from, relation_alias, except, prefix, suffix)) }}
+{% macro star(from, relation_alias=False, except=[], contains_item = none, include_or_exclude_contains_item = none, separator = ',', column_prefix = none) -%}
+    {{ return(adapter.dispatch('star', 'cc_dbt_utils')(from, relation_alias, except, contains_item, include_or_exclude_contains_item, separator, column_prefix)) }}
 {% endmacro %}
 
-{% macro default__star(from, relation_alias=False, except=[], prefix='', suffix='') -%}
-    {%- do dbt_utils._is_relation(from, 'star') -%}
-    {%- do dbt_utils._is_ephemeral(from, 'star') -%}
+{% macro default__star(from, relation_alias=False, except=[], contains_item = none, include_or_exclude_contains_item = none, separator = ',', column_prefix = none) -%}
+    
+    {%- do cc_dbt_utils._is_relation(from, 'star') -%}
 
     {#-- Prevent querying of db in parsing mode. This works because this macro does not create any new refs. #}
+
     {%- if not execute -%}
         {{ return('') }}
     {% endif %}
 
     {%- set include_cols = [] %}
     {%- set cols = adapter.get_columns_in_relation(from) -%}
-    {%- set except = except | map("lower") | list %}
+    
+    {% if contains_item is not none and include_or_exclude_contains_item is none %}
+        {%- do exceptions.raise_compiler_error("Macro " ~ macro ~ " expected a value for argument include_or_exclude_contains_item but received none: " ~ obj) -%}
+    {% endif %}
+
+    {%- set in_or_out = include_or_exclude_contains_item if include_or_exclude_contains_item is not none else [] %}
+
+
     {%- for col in cols -%}
 
-        {%- if col.column|lower not in except -%}
-            {% do include_cols.append(col.column) %}
-
+        {% if contains_item is not none and col.column not in except and contains_item|upper in col.column and in_or_out == 'include' %}
+                {% do include_cols.append(col.column) %}
+        {%- elif contains_item is not none and col.column not in except and contains_item|upper not in col.column and in_or_out == 'exclude' -%}
+                {% do include_cols.append(col.column) %}
+        {%- elif contains_item is none and col.column not in except -%} 
+                {% do include_cols.append(col.column) %}
         {%- endif %}
+
     {%- endfor %}
 
     {%- for col in include_cols %}
 
-        {%- if relation_alias %}{{ relation_alias }}.{% else %}{%- endif -%}{{ adapter.quote(col)|trim }} as {{ adapter.quote(prefix ~ col ~ suffix)|trim }}
-        {%- if not loop.last %},{{ '\n  ' }}{% endif %}
+        {%- if relation_alias %}{{ relation_alias }}.{% else %}{%- endif -%}{{ adapter.quote(col)|trim }} {%- if column_prefix %} as {{ column_prefix }}{{ col|trim }}{% else %}{%- endif -%}
+        {%- if not loop.last %}{{ separator }} {{ '\n  ' }}{% endif %}
 
     {%- endfor -%}
 {%- endmacro %}

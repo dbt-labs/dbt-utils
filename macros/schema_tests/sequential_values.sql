9fa2253fc3b55ae42e5d137a22a973bad87f0374
Armand Duijn <armand@duijn.space>
2022-01-24 20:48:27 +0100

    Refactor column name

--- macros/schema_tests/sequential_values.sql
+++ macros/schema_tests/sequential_values.sql
@@ -6,13 +6,15 @@
 
 {% macro default__test_sequential_values(model, column_name, interval=1, datepart=None) %}
 
+{% set previous_column_name = "previous_" ~ dbt_utils.slugify(column_name) %}
+
 with windowed as (
 
     select
         {{ column_name }},
         lag({{ column_name }}) over (
             order by {{ column_name }}
-        ) as _previous
+        ) as {{ previous_column_name }}
     from {{ model }}
 ),
 
@@ -21,9 +23,9 @@ validation_errors as (
         *
     from windowed
     {% if datepart %}
-    where not(cast({{ column_name }} as {{ dbt_utils.type_timestamp() }})= cast({{ dbt_utils.dateadd(datepart, interval, '_previous') }} as {{ dbt_utils.type_timestamp() }}))
+    where not(cast({{ column_name }} as {{ dbt_utils.type_timestamp() }})= cast({{ dbt_utils.dateadd(datepart, interval, previous_column_name) }} as {{ dbt_utils.type_timestamp() }}))
     {% else %}
-    where not({{ column_name }} = _previous + {{ interval }})
+    where not({{ column_name }} = {{ previous_column_name }} + {{ interval }})
     {% endif %}
 )
 

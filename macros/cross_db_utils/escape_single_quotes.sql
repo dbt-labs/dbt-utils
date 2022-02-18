a866881fd0c9329ab5048a460159f92f4456551f
Marc Dutoo <marc.dutoo@gmail.com>
2022-02-18 12:57:40 +0100

    single quote escape : added dispatched version of the macro to support bigquery & snowflake

---
+++ macros/cross_db_utils/escape_single_quotes.sql
@@ -0,0 +1,9 @@
+{% macro escape_single_quotes(expression) %}
+      {{ return(adapter.dispatch('escape_single_quotes', 'dbt_utils') (expression)) }}
+{% endmacro %}
+
+{% macro default__escape_single_quotes(expression) %}{{ expression | replace("'","''") }}{% endmacro %}
+
+{% macro snowflake__escape_single_quotes(expression) %}{{ expression | replace("'", "\'") }}{% endmacro %}
+
+{% macro bigquery__escape_single_quotes(expression) %}{{ expression | replace("'", "\'") }}{% endmacro %}
--- macros/sql/pivot.sql
+++ macros/sql/pivot.sql
@@ -69,7 +69,7 @@ Arguments:
     {{ agg }}(
       {% if distinct %} distinct {% endif %}
       case
-      when {{ column }} {{ cmp }} '{{ v | replace("'","''") }}' {# escape quote in value (in postgresql) #}
+      when {{ column }} {{ cmp }} '{{ dbt_utils.escape_single_quotes(v) }}' {# escape else ex. syntax error near : when color = 'blue's' #}
         then {{ then_value }}
       else {{ else_value }}
       end

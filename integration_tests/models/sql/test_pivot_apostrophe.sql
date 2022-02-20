58a99513172378b5d908b6c581a580cf94d73348
Joel Labes <joel.labes@dbtlabs.com>
2022-02-21 12:45:53 +1300

    Don't quote identifiers for apostrophe, to avoid BQ and SF problems

--- integration_tests/models/sql/test_pivot_apostrophe.sql
+++ integration_tests/models/sql/test_pivot_apostrophe.sql
@@ -2,16 +2,16 @@
 -- TODO: How do we make this work nicely on Snowflake too?
 
 {% if target.type == 'snowflake' %}
-    {% set column_values = ['RED', 'BLUE'] %}
+    {% set column_values = ['RED', 'BLUE', "BLUE'S"] %}
     {% set cmp = 'ilike' %}
 {% else %}
-    {% set column_values = ['red', 'blue'] %}
+    {% set column_values = ['red', 'blue', "blue's"] %}
     {% set cmp = '=' %}
 {% endif %}
 
 select
     size,
-    {{ dbt_utils.pivot('color', column_values, cmp=cmp) }}
+    {{ dbt_utils.pivot('color', column_values, cmp=cmp, quote_identifiers=False) }}
 
 from {{ ref('data_pivot') }}
 group by size

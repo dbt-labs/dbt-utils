fe2d9a75586ab33e6ade2806941bbecd3a0fded6
Joel Labes <joel.labes@dbtlabs.com>
2022-02-21 13:17:16 +1300

    Whitespace management for macros

--- macros/cross_db_utils/escape_single_quotes.sql
+++ macros/cross_db_utils/escape_single_quotes.sql
@@ -2,8 +2,14 @@
       {{ return(adapter.dispatch('escape_single_quotes', 'dbt_utils') (expression)) }}
 {% endmacro %}
 
-{% macro default__escape_single_quotes(expression) %}{{ expression | replace("'","''") }}{% endmacro %}
+{% macro default__escape_single_quotes(expression) -%}
+{{ expression | replace("'","''") }}
+{%- endmacro %}
 
-{% macro snowflake__escape_single_quotes(expression) %}{{ expression | replace("'", "\\'") }}{% endmacro %}
+{% macro snowflake__escape_single_quotes(expression) -%}
+{{ expression | replace("'", "\\'") }}
+{%- endmacro %}
 
-{% macro bigquery__escape_single_quotes(expression) %}{{ expression | replace("'", "\\'") }}{% endmacro %}
+{% macro bigquery__escape_single_quotes(expression) -%}
+{{ expression | replace("'", "\\'") }}
+{%- endmacro %}
--- macros/sql/pivot.sql
+++ macros/sql/pivot.sql
@@ -69,7 +69,7 @@ Arguments:
     {{ agg }}(
       {% if distinct %} distinct {% endif %}
       case
-      when {{ column }} {{ cmp }} '{{ dbt_utils.escape_single_quotes(v) }}' {# escape else ex. syntax error near : when color = 'blue's' #}
+      when {{ column }} {{ cmp }} '{{ dbt_utils.escape_single_quotes(v) }}'
         then {{ then_value }}
       else {{ else_value }}
       end

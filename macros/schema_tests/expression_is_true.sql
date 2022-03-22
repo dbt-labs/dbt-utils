c7c38212f708f1b414f9e327320cc775a0aa9cda
James McNeill <55981540+jpmmcneill@users.noreply.github.com>
2022-03-22 01:11:53 +0000

    Jmcneill/expression is true tweak (#507)
    * Update README.md
    
    * add some flexibility to expression_is_true execution plan and add a few new tests
    
    * catch duplicate field name exception when the expression_is_true test is invoked
    
    * expression is true - rename seeds, format sql and get rid of dupe column handler from PR comments
    
    Co-authored-by: Joel Labes <joel.labes@dbtlabs.com>

--- macros/schema_tests/expression_is_true.sql
+++ macros/schema_tests/expression_is_true.sql
@@ -7,16 +7,19 @@
 {% macro default__test_expression_is_true(model, expression, column_name, condition) %}
 
 with meet_condition as (
-    select * from {{ model }} where {{ condition }}
+    select
+      *,
+      {% if column_name is none %}
+      {{ expression }}
+      {%- else %}
+      {{ column_name }} {{ expression }}
+      {%- endif %}
+      as _dbt_utils_test_expression_passed
+    from {{ model }}
+    where {{ condition }}
 )
 
-select
-    *
-from meet_condition
-{% if column_name is none %}
-where not({{ expression }})
-{%- else %}
-where not({{ column_name }} {{ expression }})
-{%- endif %}
+select * from meet_condition
+where not(_dbt_utils_test_expression_passed)
 
 {% endmacro %}

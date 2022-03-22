8b3de4314ae41c954b0ea402aa3b076f631082da
Judah Rand <17158624+judahrand@users.noreply.github.com>
2022-03-22 08:48:46 +0000

    Test `alias` argument

--- integration_tests/models/sql/test_deduplicate.sql
+++ integration_tests/models/sql/test_deduplicate.sql
@@ -1,6 +1,21 @@
-with deduped as (
+with
 
-    {{ dbt_utils.deduplicate(ref('data_deduplicate'), group_by='user_id', order_by='version desc') | indent }}
+source as (
+    select *
+    from {{ ref('data_deduplicate') }}
+    where user_id = 1
+),
+
+deduped as (
+
+    {{
+        dbt_utils.deduplicate(
+            ref('data_deduplicate'),
+            group_by='user_id',
+            order_by='version desc',
+            alias="source"
+        ) | indent
+    }}
 
 )
 

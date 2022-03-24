1f19ed845998f020153806b2038fc61f077febf2
Pat Kearns <pat.kearns@fishtownanalytics.com>
2022-03-24 13:39:34 +1100

    too much whitespace removal
        -----------
        Actual:
        -----------
        --->"field_3"as "test_field_3"<---
    
        -----------
        Expected:
        -----------
        --->"field_3" as "test_field_3"<---

--- macros/sql/star.sql
+++ macros/sql/star.sql
@@ -13,7 +13,7 @@
 
     {%- for col in dbt_utils.get_filtered_columns_in_relation(from, except, output_lower) %}
 
-        {%- if relation_alias %}{{ relation_alias }}.{% else %}{%- endif -%}{{ adapter.quote(col)|trim }} {%- if prefix!='' or suffix!='' -%} as {{ adapter.quote(prefix ~ col ~ suffix)|trim }} {%- endif -%}
+        {%- if relation_alias %}{{ relation_alias }}.{% else %}{%- endif -%}{{ adapter.quote(col)|trim }} {%- if prefix!='' or suffix!='' %} as {{ adapter.quote(prefix ~ col ~ suffix)|trim }} {%- endif -%}
         {%- if not loop.last %},{{ '\n  ' }}{% endif %}
 
     {%- endfor -%}

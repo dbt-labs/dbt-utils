55c9a4948016801f1a5bf271c920cd7ad6dd197a
Grace Goheen <graciegoheen@gmail.com>
2022-03-30 13:24:42 -0400

    added limit functionality for other dbs

--- integration_tests/models/cross_db_utils/test_listagg.sql
+++ integration_tests/models/cross_db_utils/test_listagg.sql
@@ -14,7 +14,17 @@ calculate as (
 
     select
         group_col,
-        {{ dbt_utils.listagg('string_text', "','", "order by order_col") }} as actual
+        {{ dbt_utils.listagg('string_text', "','", "order by order_col") }} as actual,
+        1 as version
+    from data
+    group by group_col
+
+    union all
+
+    select
+        group_col,
+        {{ dbt_utils.listagg('string_text', "','", "order by order_col", 2, 'group_col') }} as actual,
+        2 as version
     from data
     group by group_col
 
@@ -26,3 +36,4 @@ select
 from calculate
 left join data_output
 on calculate.group_col = data_output.group_col
+and calculate.version = data_output.version
--- macros/cross_db_utils/listagg.sql
+++ macros/cross_db_utils/listagg.sql
@@ -1,10 +1,28 @@
-{% macro listagg(measure, delimiter_text=none, order_by_clause=none, limit_clause=none) -%}
-  {{ return(adapter.dispatch('listagg', 'dbt_utils') (measure, delimiter_text, order_by_clause, limit_clause)) }}
+{% macro listagg(measure, delimiter_text=none, order_by_clause=none, limit_num=none, group_by_col=none) -%}
+  {{ return(adapter.dispatch('listagg', 'dbt_utils') (measure, delimiter_text, order_by_clause, limit_num, group_by_col)) }}
 {%- endmacro %}
 
 
-{% macro default__listagg(measure, delimiter_text, order_by_clause, limit_clause) -%}
+{% macro default__listagg(measure, delimiter_text, order_by_clause, limit_num, group_by_col) -%}
 
+    {% if limit_num -%}
+    array_to_string(
+        array_slice(
+            array_agg(
+                {{ measure }}
+            ){% if order_by_clause -%}
+            within group ({{ order_by_clause }})
+            {%- endif %}
+            ,0
+            ,{{ limit_num }}
+        )
+        {% if delimiter_text -%}
+        ,{{ delimiter_text }}
+        {%- else %}
+        ,''
+        {%- endif %}
+        )
+    {%- else %}
     listagg(
         {{ measure }}
         {% if delimiter_text -%}
@@ -14,10 +32,11 @@
         {% if order_by_clause -%}
         within group ({{ order_by_clause }})
         {%- endif %}
+    {%- endif %}
 
 {%- endmacro %}
 
-{% macro bigquery__listagg(measure, delimiter_text, order_by_clause, limit_clause) -%}
+{% macro bigquery__listagg(measure, delimiter_text, order_by_clause, limit_num, group_by_col) -%}
 
     string_agg(
         {{ measure }}
@@ -27,15 +46,27 @@
         {% if order_by_clause -%}
         {{ order_by_clause }}
         {%- endif %}
-        {% if limit_clause -%}
-        {{ limit_clause }}
+        {% if limit_num -%}
+        limit {{ limit_num }}
         {%- endif %}
         )
 
 {%- endmacro %}
 
-{% macro postgres__listagg(measure, delimiter_text, order_by_clause, limit_clause) -%}
-
+{# in redshift if you must supply a delimiter_text #}
+{% macro postgres__listagg(measure, delimiter_text, order_by_clause, limit_num, group_by_col) -%}
+    
+    {% if limit_num -%}
+    array_to_string(
+        array_agg(
+            {{ measure }}
+            {% if order_by_clause -%}
+            {{ order_by_clause }}
+            {%- endif %}
+        )[1:{{ limit_num }}],
+        {{ delimiter_text }}
+        )
+    {%- else %}
     string_agg(
         {{ measure }},
         {{ delimiter_text }}
@@ -43,12 +74,35 @@
         {{ order_by_clause }}
         {%- endif %}
         )
+    {%- endif %}
 
 {%- endmacro %}
 
-{# redshift should use default instead of postgres #}
-{% macro redshift__listagg(measure, delimiter_text, order_by_clause, limit_clause) -%}
+{# in redshift if you supply a limit_num you must also supply a group_by_col #}
+{% macro redshift__listagg(measure, delimiter_text, order_by_clause, limit_num, group_by_col) -%}
 
-    {{ return(dbt_utils.default__listagg(measure, delimiter_text, order_by_clause, limit_clause)) }}
+    {% if limit_num and group_by_col -%}
+    listagg( -- what about distinct?
+        (
+            case when row_number() over (partition by {{ group_by_col }} {% if order_by_clause %} {{ order_by_clause }} {% endif %}) <= {{ limit_num }} then {{ measure }} end
+        )
+        {% if delimiter_text -%}
+        ,{{ delimiter_text }}
+        {%- endif %}
+        )
+        {% if order_by_clause -%}
+        within group ({{ order_by_clause }})
+        {%- endif %}
+    {%- else %}
+    listagg(
+        {{ measure }}
+        {% if delimiter_text -%}
+        ,{{ delimiter_text }}
+        {%- endif %}
+        )
+        {% if order_by_clause -%}
+        within group ({{ order_by_clause }})
+        {%- endif %}
+    {%- endif %}
 
 {%- endmacro %}

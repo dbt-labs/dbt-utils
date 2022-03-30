05812fb6e55ba9b1b267195bbbf4ead574341a4e
Grace Goheen <graciegoheen@gmail.com>
2022-03-30 14:27:37 -0400

    fixed block def control

--- macros/cross_db_utils/listagg.sql
+++ macros/cross_db_utils/listagg.sql
@@ -1,8 +1,5 @@
-{% macro listagg(measure, delimiter_text=none, order_by_clause=none, limit_num=none, group_by_col=none) -%}
-  {% if delimiter_text -%}  
+{% macro listagg(measure, delimiter_text=",", order_by_clause=none, limit_num=none, group_by_col=none) -%}
     {{ return(adapter.dispatch('listagg', 'dbt_utils') (measure, delimiter_text, order_by_clause, limit_num)) }}
-  {% else %}
-    {{ return(adapter.dispatch('listagg', 'dbt_utils') (measure, delimiter_text=",", order_by_clause, limit_num)) }}
 {%- endmacro %}
 
 {% macro default__listagg(measure, delimiter_text, order_by_clause, limit_num) -%}

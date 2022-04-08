10ced1334aae1cc36cc64897a825bcb774494f50
Joel Labes <joel.labes@dbtlabs.com>
2022-04-08 13:48:28 +1200

    Change from quotes to backticks for BQ

--- macros/sql/get_table_types_sql.sql
+++ macros/sql/get_table_types_sql.sql
@@ -20,3 +20,12 @@
                 else lower(table_type)
             end as "table_type"
 {% endmacro %}
+
+{% macro bigquery__get_table_types_sql() %}
+            case table_type
+                when 'BASE TABLE' then 'table'
+                when 'EXTERNAL TABLE' then 'external'
+                when 'MATERIALIZED VIEW' then 'materializedview'
+                else lower(table_type)
+            end as `table_type`
+{% endmacro %}

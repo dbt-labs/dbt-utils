f12df79a8e5836960179427c956a6b7c3b1ea63c
Siarhei Nekhviadovich <Siarhei_Nekhviadovich@epam.com>
2022-04-14 08:31:38 +0200

    Athena / Presto support for get tables by pattern

--- macros/sql/get_tables_by_pattern_sql.sql
+++ macros/sql/get_tables_by_pattern_sql.sql
@@ -16,6 +16,39 @@
 
 {% endmacro %}
 
+{% macro athena__get_tables_by_pattern_sql(schema_pattern, table_pattern, exclude='', database=target.database) %}
+
+{% set table_schema_like_str = "regexp_like({}, '(?i)\\A{}\\Z')".format("table_schema", schema_pattern) %}
+{% set table_name_like_str = "regexp_like({}, '(?i)\\A{}\\Z')".format("table_name", table_pattern) %}
+{% set table_name_not_like_str = "not regexp_like({}, '(?i)\\A{}\\Z')".format("table_name", exclude) %}
+
+        select distinct
+            table_schema as "table_schema",
+            table_name as "table_name",
+            {{ dbt_utils.get_table_types_sql() }}
+        from {{ database }}.information_schema.tables
+        where {{ table_schema_like_str }}
+        and {{ table_name_like_str }}
+        and {{ table_name_not_like_str }}
+
+{% endmacro %}
+
+{% macro presto__get_tables_by_pattern_sql(schema_pattern, table_pattern, exclude='', database=target.database) %}
+
+{% set table_schema_like_str = "regexp_like({}, '(?i)\\A{}\\Z')".format("table_schema", schema_pattern) %}
+{% set table_name_like_str = "regexp_like({}, '(?i)\\A{}\\Z')".format("table_name", table_pattern) %}
+{% set table_name_not_like_str = "not regexp_like({}, '(?i)\\A{}\\Z')".format("table_name", exclude) %}
+
+        select distinct
+            table_schema as "table_schema",
+            table_name as "table_name",
+            {{ dbt_utils.get_table_types_sql() }}
+        from {{ database }}.information_schema.tables
+        where {{ table_schema_like_str }}
+        and {{ table_name_like_str }}
+        and {{ table_name_not_like_str }}
+
+{% endmacro %}
 
 {% macro bigquery__get_tables_by_pattern_sql(schema_pattern, table_pattern, exclude='', database=target.database) %}
 

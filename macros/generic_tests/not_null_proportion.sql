31cb6ddefcbf80bd23d58bd3b04de6d0fb047b96
Siarhei Nekhviadovich <Siarhei_Nekhviadovich@epam.com>
2022-04-15 15:39:25 +0200

    Not Null Proportion support for Athena / Presto

--- macros/generic_tests/not_null_proportion.sql
+++ macros/generic_tests/not_null_proportion.sql
@@ -24,3 +24,50 @@ select
 from validation_errors
 
 {% endmacro %}
+
+{% macro athena__test_not_null_proportion(model) %}
+
+{% set column_name = kwargs.get('column_name', kwargs.get('arg')) %}
+{% set at_least = kwargs.get('at_least', kwargs.get('arg')) %}
+{% set at_most = kwargs.get('at_most', kwargs.get('arg', 1)) %}
+
+with validation as (
+  select
+    sum(case when {{ column_name }} is null then 0 else 1 end) / cast(count(*) as double) as not_null_proportion
+  from {{ model }}
+),
+validation_errors as (
+  select
+    not_null_proportion
+  from validation
+  where not_null_proportion < {{ at_least }} or not_null_proportion > {{ at_most }}
+)
+select
+  *
+from validation_errors
+
+{% endmacro %}
+
+{% macro presto__test_not_null_proportion(model) %}
+
+{% set column_name = kwargs.get('column_name', kwargs.get('arg')) %}
+{% set at_least = kwargs.get('at_least', kwargs.get('arg')) %}
+{% set at_most = kwargs.get('at_most', kwargs.get('arg', 1)) %}
+
+with validation as (
+  select
+    sum(case when {{ column_name }} is null then 0 else 1 end) / cast(count(*) as double) as not_null_proportion
+  from {{ model }}
+),
+validation_errors as (
+  select
+    not_null_proportion
+  from validation
+  where not_null_proportion < {{ at_least }} or not_null_proportion > {{ at_most }}
+)
+select
+  *
+from validation_errors
+
+{% endmacro %}
+

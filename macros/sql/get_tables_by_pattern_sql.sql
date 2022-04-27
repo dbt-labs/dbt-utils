cd2f6953bb3132037279571d47abfb1a34e11230
Silvio Luiz <silvio.luiz@gmail.com>
2022-04-27 15:32:49 -0300

    Update get_tables_by_pattern_sql.sql
    Create  specific athena macro because ilike is not a valid function

--- macros/sql/get_tables_by_pattern_sql.sql
+++ macros/sql/get_tables_by_pattern_sql.sql
@@ -16,6 +16,19 @@
 
 {% endmacro %}
 
+{% macro athena__get_tables_by_pattern_sql(schema_pattern, table_pattern, exclude='', database=target.database) %}
+
+        select distinct
+            table_schema as "table_schema",
+            table_name as "table_name",
+            {{ dbt_utils.get_table_types_sql() }}
+        from {{ database }}.information_schema.tables
+        where lower(table_schema) like lower('{{ schema_pattern }}')
+        and lower(table_name) like lower ('{{ table_pattern }}')
+        and lower(table_name) not like lower ('{{ exclude }}')
+
+{% endmacro %}
+
 
 {% macro bigquery__get_tables_by_pattern_sql(schema_pattern, table_pattern, exclude='', database=target.database) %}
 

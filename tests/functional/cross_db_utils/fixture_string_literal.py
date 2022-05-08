ade997bbf05f7d981d420b6238bb829768ddf415
Doug Beatty <doug.beatty@dbtlabs.com>
2022-05-08 17:52:25 -0600

    Functional tests for string_literal macro

--- tests/functional/cross_db_utils/fixture_string_literal.py
+++ tests/functional/cross_db_utils/fixture_string_literal.py
@@ -1,27 +1,11 @@
 
-
 # string_literal
 
-# TODO - implement expected results here
-seeds__data_string_literal_csv = """todo,result
-TODO,1
-"""
-
-
 models__test_string_literal_sql = """
-with data as (
-
-    select * from {{ ref('data_string_literal') }}
-
-)
-
-# TODO - implement actual logic here
-select
-
-    1 actual,
-    result as expected
-
-from data
+select {{ dbt_utils.string_literal("abc") }} as actual, 'abc' as expected union all
+select {{ dbt_utils.string_literal("1") }} as actual, '1' as expected union all
+select {{ dbt_utils.string_literal("") }} as actual, '' as expected union all
+select {{ dbt_utils.string_literal(none) }} as actual, 'None' as expected
 """
 
 
--- tests/functional/cross_db_utils/test_string_literal.py
+++ tests/functional/cross_db_utils/test_string_literal.py
@@ -1,17 +1,12 @@
 import pytest
 from tests.functional.cross_db_utils.base_cross_db_macro import BaseCrossDbMacro
 from tests.functional.cross_db_utils.fixture_string_literal import (
-    seeds__data_string_literal_csv,
     models__test_string_literal_sql,
     models__test_string_literal_yml,
 )
 
 
 class BaseStringLiteral(BaseCrossDbMacro):
-    @pytest.fixture(scope="class")
-    def seeds(self):
-        return {"data_string_literal.csv": seeds__data_string_literal_csv}
-
     @pytest.fixture(scope="class")
     def models(self):
         return {
@@ -20,6 +15,5 @@ class BaseStringLiteral(BaseCrossDbMacro):
         }
 
 
-@pytest.mark.skip(reason="TODO - implement this test")
 class TestStringLiteral(BaseStringLiteral):
     pass

1820d762f654f6fd84f88cf34ae6f8ef9ee4f7be
Doug Beatty <doug.beatty@dbtlabs.com>
2022-05-08 14:52:05 -0600

    Functional tests for current_timestamp_in_utc macro

--- tests/functional/cross_db_utils/fixture_current_timestamp_in_utc.py
+++ tests/functional/cross_db_utils/fixture_current_timestamp_in_utc.py
@@ -1,27 +1,11 @@
 
-
 # current_timestamp_in_utc
 
-# TODO - implement expected results here
-seeds__data_current_timestamp_in_utc_csv = """todo,result
-TODO,1
-"""
-
-
+# TODO how can we test this better?
 models__test_current_timestamp_in_utc_sql = """
-with data as (
-
-    select * from {{ ref('data_current_timestamp_in_utc') }}
-
-)
-
-# TODO - implement actual logic here
 select
-
-    1 actual,
-    result as expected
-
-from data
+    {{ dbt_utils.current_timestamp_in_utc() }} as actual,
+    {{ dbt_utils.current_timestamp_in_utc() }} as expected
 """
 
 
--- tests/functional/cross_db_utils/test_current_timestamp_in_utc.py
+++ tests/functional/cross_db_utils/test_current_timestamp_in_utc.py
@@ -1,17 +1,12 @@
 import pytest
 from tests.functional.cross_db_utils.base_cross_db_macro import BaseCrossDbMacro
 from tests.functional.cross_db_utils.fixture_current_timestamp_in_utc import (
-    seeds__data_current_timestamp_in_utc_csv,
     models__test_current_timestamp_in_utc_sql,
     models__test_current_timestamp_in_utc_yml,
 )
 
 
 class BaseCurrentTimestampInUtc(BaseCrossDbMacro):
-    @pytest.fixture(scope="class")
-    def seeds(self):
-        return {"data_current_timestamp_in_utc.csv": seeds__data_current_timestamp_in_utc_csv}
-
     @pytest.fixture(scope="class")
     def models(self):
         return {
@@ -20,6 +15,5 @@ class BaseCurrentTimestampInUtc(BaseCrossDbMacro):
         }
 
 
-@pytest.mark.skip(reason="TODO - implement this test")
 class TestCurrentTimestampInUtc(BaseCurrentTimestampInUtc):
     pass

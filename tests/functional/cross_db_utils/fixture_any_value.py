d4cbc0d6b8c2c9a0d24686386d9c0e7a0f4dcace
Doug Beatty <doug.beatty@dbtlabs.com>
2022-05-08 13:51:15 -0600

    Conform any_value macro to actual/expected test pattern

--- tests/functional/cross_db_utils/fixture_any_value.py
+++ tests/functional/cross_db_utils/fixture_any_value.py
@@ -1,8 +1,17 @@
 
-
 # any_value
 
-seeds__data_any_value_csv = """key_name,static_col,num_rows
+seeds__data_any_value_csv = """id,key_name,static_col
+1,abc,dbt
+2,abc,dbt
+3,jkl,dbt
+4,jkl,dbt
+5,jkl,dbt
+6,xyz,test
+"""
+
+
+seeds__data_any_value_expected_csv = """key_name,static_col,num_rows
 abc,dbt,2
 jkl,dbt,3
 xyz,test,1
@@ -10,25 +19,34 @@ xyz,test,1
 
 
 models__test_any_value_sql = """
-with some_model as (
-    select 1 as id, 'abc' as key_name, 'dbt' as static_col union all
-    select 2 as id, 'abc' as key_name, 'dbt' as static_col union all
-    select 3 as id, 'jkl' as key_name, 'dbt' as static_col union all
-    select 4 as id, 'jkl' as key_name, 'dbt' as static_col union all
-    select 5 as id, 'jkl' as key_name, 'dbt' as static_col union all
-    select 6 as id, 'xyz' as key_name, 'test' as static_col
+with data as (
+
+    select * from {{ ref('data_any_value') }}
+
 ),
 
-final as (
+data_output as (
+
+    select * from {{ ref('data_any_value_expected') }}
+
+),
+
+calculate as (
     select
         key_name,
         {{ dbt_utils.any_value('static_col') }} as static_col,
         count(id) as num_rows
-    from some_model
+    from data
     group by key_name
 )
 
-select * from final
+select
+    calculate.num_rows as actual,
+    data_output.num_rows as expected
+from calculate
+left join data_output
+on calculate.key_name = data_output.key_name
+and calculate.static_col = data_output.static_col
 """
 
 
@@ -37,6 +55,7 @@ version: 2
 models:
   - name: test_any_value
     tests:
-      - dbt_utils.equality:
-          compare_model: ref('data_any_value')
+      - assert_equal:
+          actual: actual
+          expected: expected
 """

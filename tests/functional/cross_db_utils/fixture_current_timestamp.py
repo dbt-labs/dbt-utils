7e2ea34028d5e5557f538487e5710475979ccb59
Doug Beatty <doug.beatty@dbtlabs.com>
2022-05-08 14:47:06 -0600

    Functional tests for current_timestamp macro

--- tests/functional/cross_db_utils/fixture_current_timestamp.py
+++ tests/functional/cross_db_utils/fixture_current_timestamp.py
@@ -1,27 +1,11 @@
 
-
 # current_timestamp
 
-# TODO - implement expected results here
-seeds__data_current_timestamp_csv = """todo,result
-TODO,1
-"""
-
-
+# TODO how can we test this better?
 models__test_current_timestamp_sql = """
-with data as (
-
-    select * from {{ ref('data_current_timestamp') }}
-
-)
-
-# TODO - implement actual logic here
 select
-
-    1 actual,
-    result as expected
-
-from data
+    {{ dbt_utils.current_timestamp() }} as actual,
+    {{ dbt_utils.current_timestamp() }} as expected
 """
 
 
--- tests/functional/cross_db_utils/test_current_timestamp.py
+++ tests/functional/cross_db_utils/test_current_timestamp.py
@@ -1,16 +1,12 @@
 import pytest
 from tests.functional.cross_db_utils.base_cross_db_macro import BaseCrossDbMacro
 from tests.functional.cross_db_utils.fixture_current_timestamp import (
-    seeds__data_current_timestamp_csv,
     models__test_current_timestamp_sql,
     models__test_current_timestamp_yml,
 )
 
 
 class BaseCurrentTimestamp(BaseCrossDbMacro):
-    @pytest.fixture(scope="class")
-    def seeds(self):
-        return {"data_current_timestamp.csv": seeds__data_current_timestamp_csv}
 
     @pytest.fixture(scope="class")
     def models(self):
@@ -20,6 +16,5 @@ class BaseCurrentTimestamp(BaseCrossDbMacro):
         }
 
 
-@pytest.mark.skip(reason="TODO - implement this test")
 class TestCurrentTimestamp(BaseCurrentTimestamp):
     pass

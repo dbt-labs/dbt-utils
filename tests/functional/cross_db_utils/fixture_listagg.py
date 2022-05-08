c3f6235dc5c5a01a9832fa4beda61441dca8aaed
Doug Beatty <doug.beatty@dbtlabs.com>
2022-05-08 11:32:50 -0600

    Functional tests for listagg macro

--- tests/functional/cross_db_utils/fixture_listagg.py
+++ tests/functional/cross_db_utils/fixture_listagg.py
@@ -1,10 +1,29 @@
 
-
 # listagg
 
-# TODO - implement expected results here
-seeds__data_listagg_csv = """todo,result
-TODO,1
+seeds__data_listagg_csv = """group_col,string_text,order_col
+1,a,1
+1,b,2
+1,c,3
+2,a,2
+2,1,1
+2,p,3
+3,g,1
+3,g,2
+3,g,3
+"""
+
+
+seeds__data_listagg_output_csv = """group_col,expected,version
+1,"a_|_b_|_c",bottom_ordered
+2,"1_|_a_|_p",bottom_ordered
+3,"g_|_g_|_g",bottom_ordered
+1,"a_|_b",bottom_ordered_limited
+2,"1_|_a",bottom_ordered_limited
+3,"g_|_g",bottom_ordered_limited
+3,"g, g, g",comma_whitespace_unordered
+3,"g",distinct_comma
+3,"g,g,g",no_params
 """
 
 
@@ -13,16 +32,71 @@ with data as (
 
     select * from {{ ref('data_listagg') }}
 
+),
+
+data_output as (
+
+    select * from {{ ref('data_listagg_output') }}
+
+),
+
+calculate as (
+
+    select
+        group_col,
+        {{ dbt_utils.listagg('string_text', "'_|_'", "order by order_col") }} as actual,
+        'bottom_ordered' as version
+    from data
+    group by group_col
+
+    union all
+
+    select
+        group_col,
+        {{ dbt_utils.listagg('string_text', "'_|_'", "order by order_col", 2) }} as actual,
+        'bottom_ordered_limited' as version
+    from data
+    group by group_col
+
+    union all
+
+    select
+        group_col,
+        {{ dbt_utils.listagg('string_text', "', '") }} as actual,
+        'comma_whitespace_unordered' as version
+    from data
+    where group_col = 3
+    group by group_col
+
+    union all
+
+    select
+        group_col,
+        {{ dbt_utils.listagg('DISTINCT string_text', "','") }} as actual,
+        'distinct_comma' as version
+    from data
+    where group_col = 3
+    group by group_col
+
+    union all
+
+    select
+        group_col,
+        {{ dbt_utils.listagg('string_text') }} as actual,
+        'no_params' as version
+    from data
+    where group_col = 3
+    group by group_col
+
 )
 
-# TODO - implement actual logic here
 select
-
-    1 actual,
-    result as expected
-
-from data
-"""
+    calculate.actual,
+    data_output.expected
+from calculate
+left join data_output
+on calculate.group_col = data_output.group_col
+and calculate.version = data_output.version"""
 
 
 models__test_listagg_yml = """
--- tests/functional/cross_db_utils/test_listagg.py
+++ tests/functional/cross_db_utils/test_listagg.py
@@ -2,6 +2,7 @@ import pytest
 from tests.functional.cross_db_utils.base_cross_db_macro import BaseCrossDbMacro
 from tests.functional.cross_db_utils.fixture_listagg import (
     seeds__data_listagg_csv,
+    seeds__data_listagg_output_csv,
     models__test_listagg_sql,
     models__test_listagg_yml,
 )
@@ -10,7 +11,10 @@ from tests.functional.cross_db_utils.fixture_listagg import (
 class BaseListagg(BaseCrossDbMacro):
     @pytest.fixture(scope="class")
     def seeds(self):
-        return {"data_listagg.csv": seeds__data_listagg_csv}
+        return {
+            "data_listagg.csv": seeds__data_listagg_csv,
+            "data_listagg_output.csv": seeds__data_listagg_output_csv,
+        }
 
     @pytest.fixture(scope="class")
     def models(self):
@@ -20,6 +24,5 @@ class BaseListagg(BaseCrossDbMacro):
         }
 
 
-@pytest.mark.skip(reason="TODO - implement this test")
 class TestListagg(BaseListagg):
     pass

e565c9cf902d5985427c0e98125cc1e92e96edcf
Doug Beatty <doug.beatty@dbtlabs.com>
2022-05-10 10:03:46 -0600

    Refactor the two variants for escaping single quotes into two different test cases

--- tests/functional/cross_db_utils/fixture_escape_single_quotes.py
+++ tests/functional/cross_db_utils/fixture_escape_single_quotes.py
@@ -1,17 +1,16 @@
 
 # escape_single_quotes
 
-# Postgres and Redshift
-models__test_escape_single_quotes_sql = """
+models__test_escape_single_quotes_quote_sql = """
 select '{{ dbt_utils.escape_single_quotes("they're") }}' as actual, 'they''re' as expected union all
 select '{{ dbt_utils.escape_single_quotes("they are") }}' as actual, 'they are' as expected
 """
 
-# Snowflake and BigQuery
-# The expected literal is actually 'they\'re', but we need to escape the backslash
-models__test_escape_single_quotes_sql_snowflake_bigquery = """
+
+# The expected literal is 'they\'re'. The second backslash is to escape it from Python.
+models__test_escape_single_quotes_backslash_sql = """
 select '{{ dbt_utils.escape_single_quotes("they're") }}' as actual, 'they\\'re' as expected union all
-select '{{ dbt_utils.escape_single_quotes("they are") }}' as actual, 'they are not' as expected
+select '{{ dbt_utils.escape_single_quotes("they are") }}' as actual, 'they are' as expected
 """
 
 
--- tests/functional/cross_db_utils/test_escape_single_quotes.py
+++ tests/functional/cross_db_utils/test_escape_single_quotes.py
@@ -1,43 +1,45 @@
 import pytest
 from tests.functional.cross_db_utils.base_cross_db_macro import BaseCrossDbMacro
 from tests.functional.cross_db_utils.fixture_escape_single_quotes import (
-    models__test_escape_single_quotes_sql,
-    models__test_escape_single_quotes_sql_snowflake_bigquery,
+    models__test_escape_single_quotes_quote_sql,
+    models__test_escape_single_quotes_backslash_sql,
     models__test_escape_single_quotes_yml,
 )
 
 
-class BaseEscapeSingleQuotes(BaseCrossDbMacro):
+class BaseEscapeSingleQuotesQuote(BaseCrossDbMacro):
     @pytest.fixture(scope="class")
     def models(self):
         return {
             "test_escape_single_quotes.yml": models__test_escape_single_quotes_yml,
-            "test_escape_single_quotes.sql": models__test_escape_single_quotes_sql,
+            "test_escape_single_quotes.sql": models__test_escape_single_quotes_quote_sql,
+        }
+
+
+class BaseEscapeSingleQuotesBackslash(BaseCrossDbMacro):
+    @pytest.fixture(scope="class")
+    def models(self):
+        return {
+            "test_escape_single_quotes.yml": models__test_escape_single_quotes_yml,
+            "test_escape_single_quotes.sql": models__test_escape_single_quotes_backslash_sql,
         }
 
 
 @pytest.mark.only_profile("postgres")
-class TestEscapeSingleQuotesPostgres(BaseEscapeSingleQuotes):
+class TestEscapeSingleQuotesPostgres(BaseEscapeSingleQuotesQuote):
     pass
 
 
 @pytest.mark.only_profile("redshift")
-class TestEscapeSingleQuotesRedshift(BaseEscapeSingleQuotes):
+class TestEscapeSingleQuotesRedshift(BaseEscapeSingleQuotesQuote):
     pass
 
 
 @pytest.mark.only_profile("snowflake")
-class TestEscapeSingleQuotesSnowflake(BaseEscapeSingleQuotes):
-    def models(self):
-        return {
-            "test_escape_single_quotes.yml": models__test_escape_single_quotes_yml,
-            "test_escape_single_quotes.sql": models__test_escape_single_quotes_sql_snowflake_bigquery,
-        }
+class TestEscapeSingleQuotesSnowflake(BaseEscapeSingleQuotesBackslash):
+    pass
+
 
 @pytest.mark.only_profile("bigquery")
-class TestEscapeSingleQuotesBigQuery(BaseEscapeSingleQuotes):
-    def models(self):
-        return {
-            "test_escape_single_quotes.yml": models__test_escape_single_quotes_yml,
-            "test_escape_single_quotes.sql": models__test_escape_single_quotes_sql_snowflake_bigquery,
-        }
+class TestEscapeSingleQuotesBigQuery(BaseEscapeSingleQuotesBackslash):
+    pass

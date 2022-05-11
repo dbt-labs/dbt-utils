224cbdb751f958da179e5c9ce1cba7f09ddba2be
Elize Papineau <elize.papineau@dbtlabs.com>
2022-05-11 16:20:07 -0700

    Add integration test for get_column_values where argument

---
+++ integration_tests/models/sql/test_get_column_values_where.sql
@@ -0,0 +1,31 @@
+
+{% set column_values = dbt_utils.get_column_values(ref('data_get_column_values_where'), 'field', default=[], order_by="field", where="condition='left'") %}
+
+
+{% if target.type == 'snowflake' %}
+
+select
+    {% for val in column_values -%}
+
+        sum(case when field = '{{ val }}' then 1 else 0 end) as count_{{ val }}
+        {%- if not loop.last %},{% endif -%}
+
+    {%- endfor %}
+
+from {{ ref('data_get_column_values_where') }}
+where condition = 'left'
+
+{% else %}
+
+select
+    {% for val in column_values -%}
+
+        {{dbt_utils.safe_cast("sum(case when field = '" ~ val ~ "' then 1 else 0 end)", dbt_utils.type_string()) }} as count_{{ val }}
+        {%- if not loop.last %},{% endif -%}
+
+    {%- endfor %}
+
+from {{ ref('data_get_column_values_where') }}
+where condition = 'left'
+
+{% endif %}

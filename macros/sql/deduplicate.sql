13a72b9e73f17684ade6fa61e36cf184a80d3fa9
Judah Rand <17158624+judahrand@users.noreply.github.com>
2022-05-16 17:56:56 +0100

    Fix BigQuery name collision

--- macros/sql/deduplicate.sql
+++ macros/sql/deduplicate.sql
@@ -107,13 +107,16 @@ The {{ model.package_name }}.{{ model.name }} model triggered this warning.
 #}
 {%- macro bigquery__deduplicate(relation, partition_by, order_by) -%}
 
-    select
-        array_agg (
-            original
-            order by {{ order_by }}
-            limit 1
-        )[offset(0)].*
-    from {{ relation }} as original
-    group by {{ partition_by }}
+    select unique.*
+    from (
+        select
+            array_agg (
+                original
+                order by {{ order_by }}
+                limit 1
+            )[offset(0)] unique
+        from {{ relation }} original
+        group by {{ partition_by }}
+    )
 
 {%- endmacro -%}

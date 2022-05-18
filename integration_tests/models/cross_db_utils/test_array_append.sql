3918e5376c6e7bb748d9c7cf8b26ea85983e5e80
Grace Goheen <graciegoheen@gmail.com>
2022-05-18 15:02:22 -0400

    replaced {} with [] for matching in postgres

--- integration_tests/models/cross_db_utils/test_array_append.sql
+++ integration_tests/models/cross_db_utils/test_array_append.sql
@@ -10,12 +10,22 @@ with data as (
     left join {{ ref('data_create_array') }} as data_create_array
     on data_array_append.array_as_string = data_create_array.result_as_string
 
+),
+
+appended_array as (
+
+    select
+        {{ dbt_utils.array_append(dbt_utils.create_array(['num_input_1', 'num_input_2', 'num_input_3']), 'element') }} as array_actual,
+        cast({{ dbt_utils.array_append(dbt_utils.create_array(['num_input_1', 'num_input_2', 'num_input_3']), 'element') }} as {{ dbt_utils.type_string() }}) as actual,
+        result_as_string as expected
+    from data
+
 )
 
+-- when casting as array to string, postgres uses {} (ex: {1,2,3}) while other dbs use [] (ex: [1,2,3])
+-- we need to cast the arrays to strings in order to compare them to the output in our seed file  
 select
-
-    {{ dbt_utils.array_append(dbt_utils.create_array(['num_input_1', 'num_input_2', 'num_input_3']), 'element') }} as array_actual,
-    cast({{ dbt_utils.array_append(dbt_utils.create_array(['num_input_1', 'num_input_2', 'num_input_3']), 'element') }} as {{ dbt_utils.type_string() }}) as actual,
-    result_as_string as expected
-
-from data
+    array_actual,
+    {{ dbt_utils.replace(dbt_utils.replace('actual',"'}'","']'"),"'{'","'['") }} as actual,
+    expected
+from appended_array
--- integration_tests/models/cross_db_utils/test_create_array.sql
+++ integration_tests/models/cross_db_utils/test_create_array.sql
@@ -2,11 +2,22 @@ with data as (
 
     select * from {{ ref('data_create_array') }}
 
+),
+
+create_array as (
+    select
+        {{ dbt_utils.create_array(['num_input_1', 'num_input_2', 'num_input_3']) }} as array_actual,
+        cast({{ dbt_utils.create_array(['num_input_1', 'num_input_2', 'num_input_3']) }} as {{ dbt_utils.type_string() }}) as actual,
+        result_as_string as expected
+
+    from data
+
 )
 
+-- when casting as array to string, postgres uses {} (ex: {1,2,3}) while other dbs use [] (ex: [1,2,3])
+-- we need to cast the arrays to strings in order to compare them to the output in our seed file  
 select
-    {{ dbt_utils.create_array(['num_input_1', 'num_input_2', 'num_input_3']) }} as array_actual,
-    cast({{ dbt_utils.create_array(['num_input_1', 'num_input_2', 'num_input_3']) }} as {{ dbt_utils.type_string() }}) as actual,
-    result_as_string as expected
-
-from data
+    array_actual,
+    {{ dbt_utils.replace(dbt_utils.replace('actual',"'}'","']'"),"'{'","'['") }} as actual,
+    expected
+from create_array

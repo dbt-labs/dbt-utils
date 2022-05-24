a950efab6086dc961813cadad5c67ecb40663dcb
Doug Beatty <doug.beatty@dbtlabs.com>
2022-05-24 14:44:40 -0600

    Only support dbt-core >= 1.2

--- macros/cross_db_utils/datediff.sql
+++ macros/cross_db_utils/datediff.sql
@@ -1,5 +1,3 @@
------- IF WE DON'T CARE ABOUT BACKWARDS COMPATIBILITY --------
-
 {% macro datediff(first_date, second_date, datepart) %}
   {{ return(adapter.dispatch('datediff', 'dbt')(first_date, second_date, datepart)) }}
 {% endmacro %}

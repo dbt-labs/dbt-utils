33b9d1bbc963a3b09e63d1841be0460f4c4a6f9d
Jeremy Cohen <jeremy@dbtlabs.com>
2022-05-31 13:34:29 +0200

    Experiment with adding functional tests

--- run_functional_test.sh
+++ run_functional_test.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
+<<<<<<< HEAD
 python3 -m pytest tests/functional --profile $1
+=======
+if [[ ! -f $VENV ]]; then
+    python3 -m venv venv
+    . $VENV
+
+    pip install --upgrade pip setuptools
+    pip install --pre "dbt-$1" -r dev-requirements.txt
+fi
+
+. $VENV
+python3 -m pytest tests/functional -n4 --profile $1
+>>>>>>> 9a245dc (Experiment with adding functional tests)

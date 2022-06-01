3bea8faed55f40fe7de69cb877a3c7245100c990
Jeremy Cohen <jeremy@dbtlabs.com>
2022-06-01 21:25:36 +0200

    Try to fix pip install

--- run_functional_test.sh
+++ run_functional_test.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-<<<<<<< HEAD
-python3 -m pytest tests/functional --profile $1
-=======
 if [[ ! -f $VENV ]]; then
     python3 -m venv venv
     . $VENV
 
-    pip install --upgrade pip setuptools
-    pip install --pre "dbt-$1" -r dev-requirements.txt
+    python3 -m pip install --upgrade pip setuptools
+    python3 -m pip install --pre "dbt-$1" -r dev-requirements.txt
 fi
 
 . $VENV
 python3 -m pytest tests/functional -n4 --profile $1
->>>>>>> 9a245dc (Experiment with adding functional tests)

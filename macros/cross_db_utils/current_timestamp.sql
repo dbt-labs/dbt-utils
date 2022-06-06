81ec92efc159d50e86ebc7ec65ad8fd3266788e8
Doug Beatty <doug.beatty@dbtlabs.com>
2022-06-06 16:40:03 -0600

    Split out the `current_timestamp` work into its own separate effort (#599)

--- macros/cross_db_utils/current_timestamp.sql
+++ macros/cross_db_utils/current_timestamp.sql
@@ -5,19 +5,3 @@
 {% macro current_timestamp_in_utc() -%}
   {{ return(adapter.dispatch('current_timestamp_in_utc', 'dbt')()) }}
 {%- endmacro %}
-
-{% macro current_timestamp__original() -%}
-  {{ return(adapter.dispatch('current_timestamp__original', 'dbt_utils')()) }}
-{%- endmacro %}
-
-{% macro default__current_timestamp__original() %}
-    current_timestamp::{{dbt_utils.type_timestamp()}}
-{% endmacro %}
-
-{% macro redshift__current_timestamp__original() %}
-    getdate()
-{% endmacro %}
-
-{% macro bigquery__current_timestamp__original() %}
-    current_timestamp
-{% endmacro %}

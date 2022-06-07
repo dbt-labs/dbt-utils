91a0a1ebde66197b0f06f2ab585925a5a2415be6
Doug Beatty <doug.beatty@dbtlabs.com>
2022-06-06 18:05:28 -0600

    Reinstate original `current_timestamp` implementation

--- macros/cross_db_utils/current_timestamp.sql
+++ macros/cross_db_utils/current_timestamp.sql
@@ -1,17 +1,21 @@
-{# This is here for backwards compatibility only #}
-
 {% macro current_timestamp() -%}
   {{ return(adapter.dispatch('current_timestamp', 'dbt_utils')()) }}
 {%- endmacro %}
 
-{% macro default__current_timestamp() -%}
-  {{ return(adapter.dispatch('current_timestamp', 'dbt')()) }}
-{%- endmacro %}
+{% macro default__current_timestamp() %}
+    current_timestamp::{{dbt_utils.type_timestamp()}}
+{% endmacro %}
+
+{% macro redshift__current_timestamp() %}
+    getdate()
+{% endmacro %}
+
+{% macro bigquery__current_timestamp() %}
+    current_timestamp
+{% endmacro %}
+
+{# This is here for backwards compatibility only #}
 
 {% macro current_timestamp_in_utc() -%}
-  {{ return(adapter.dispatch('current_timestamp_in_utc', 'dbt_utils')()) }}
-{%- endmacro %}
-
-{% macro default__current_timestamp_in_utc() -%}
   {{ return(adapter.dispatch('current_timestamp_in_utc', 'dbt')()) }}
 {%- endmacro %}

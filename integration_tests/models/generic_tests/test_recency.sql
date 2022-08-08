006b3cf06229efb2033b5d6af7973331768a3943
Emily Riederer <emilyriederer@gmail.com>
2022-08-07 19:31:35 -0500

    Add integration tests for group_by_columns macro args

--- integration_tests/models/generic_tests/test_recency.sql
+++ integration_tests/models/generic_tests/test_recency.sql
@@ -2,11 +2,15 @@
 {% if target.type == 'postgres' %}
 
 select
+    'a' as col1,
+    'b' as col2,
     {{ dbt_utils.date_trunc('day', dbt_utils.current_timestamp()) }} as today
 
 {% else %}
 
 select
+    'a' as col1,
+    'b' as col2,
     cast({{ dbt_utils.date_trunc('day', dbt_utils.current_timestamp()) }} as datetime) as today
     
 {% endif %}

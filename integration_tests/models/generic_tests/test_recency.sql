772e3c421c447b6d43b5cf6993157ec1041c2ebd
Emily Riederer <emilyriederer@gmail.com>
2022-08-08 18:17:21 -0500

    change fake data in test_recency to numeric

--- integration_tests/models/generic_tests/test_recency.sql
+++ integration_tests/models/generic_tests/test_recency.sql
@@ -2,15 +2,15 @@
 {% if target.type == 'postgres' %}
 
 select
-    'a' as col1,
-    'b' as col2,
+    1 as col1,
+    2 as col2,
     {{ dbt_utils.date_trunc('day', dbt_utils.current_timestamp()) }} as today
 
 {% else %}
 
 select
-    'a' as col1,
-    'b' as col2,
+    1 as col1,
+    2 as col2,
     cast({{ dbt_utils.date_trunc('day', dbt_utils.current_timestamp()) }} as datetime) as today
     
 {% endif %}

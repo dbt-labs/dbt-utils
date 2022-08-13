31f04268f82314799811585574681f08e19be7da
Vladimir Terzic <vlad@terzic.net>
2022-08-13 15:14:24 -0600

    add oracle flavor of date_trunc
    supports day, month, week, quarter, year...to support dbt-metric functionality. 
    if something else it picked as an option, compiler error is raised like postgress datediff macro

--- macros/cross_db_utils/date_trunc.sql
+++ macros/cross_db_utils/date_trunc.sql
@@ -6,3 +6,20 @@
   {% do dbt_utils.xdb_deprecation_warning('date_trunc', model.package_name, model.name) %}
   {{ return(adapter.dispatch('date_trunc', 'dbt') (datepart, date)) }}
 {%- endmacro %}
+
+{#-/* reference: https://www.techonthenet.com/oracle/functions/trunc_date.php */#}
+{% macro oracle__date_trunc(datepart, date) -%}
+{%- if datepart == 'day' -%}
+    trunc({{date}}, 'DDD')  
+{%- elif datepart == 'week' -%}
+    trunc({{date}}, 'WW')  
+{%- elif datepart == 'month' -%}
+    trunc({{date}}, 'MON') 
+{%- elif datepart == 'quarter' -%}
+    trunc({{date}}, 'Q')        
+{%- elif datepart == 'year' -%}
+    trunc({{date}}, 'YYYY')
+{% else %}
+        {{ exceptions.raise_compiler_error("Unsupported datepart for macro date_trunc in oracle: {!r}".format(datepart)) }}    
+{%- endif -%}    
+{%- endmacro %}

6e9515610ce0c3e8e9dbcc5b752acec482b9bc51
Elize Papineau <elize.papineau@dbtlabs.com>
2022-08-25 12:13:48 -0700

    Update union statements to union all for BQ compatibility

--- integration_tests/models/sql/test_not_empty_string_failing.sql
+++ integration_tests/models/sql/test_not_empty_string_failing.sql
@@ -3,15 +3,15 @@
 with blank_data as (
 
     select 1 as id, 'not an empty string' as string
-    union
+    union all
     select 2 as id, 'also not an empty string' as string
-    union
+    union all
     select 3 as id, 'string with trailing whitespace  ' as string
-    union
+    union all
     select 4 as id, '   ' as string
-    union
+    union all
     select 5 as id, '' as string
-    union
+    union all
     select 6 as id, null as string
 
 )

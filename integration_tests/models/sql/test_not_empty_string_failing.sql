d8664759856e9be63d2c896f5e28aca2dd113003
Elize Papineau <elize.papineau@dbtlabs.com>
2022-09-13 13:25:23 -0700

    Add test for trim_whitespace=False option

--- integration_tests/models/sql/test_not_empty_string_failing.sql
+++ integration_tests/models/sql/test_not_empty_string_failing.sql
@@ -2,17 +2,39 @@
 
 with blank_data as (
 
-    select 1 as id, 'not an empty string' as string
+    select
+        1 as id,
+        'not an empty string' as string_trim_whitespace_true
+
     union all
-    select 2 as id, 'also not an empty string' as string
+
+    select
+        2 as id,
+        'also not an empty string' as string_trim_whitespace_true
+
     union all
-    select 3 as id, 'string with trailing whitespace  ' as string
+
+    select
+        3 as id,
+        'string with trailing whitespace  ' as string_trim_whitespace_true
+
     union all
-    select 4 as id, '   ' as string
+
+    select
+        4 as id,
+        '   ' as string_trim_whitespace_true
+
     union all
-    select 5 as id, '' as string
+
+    select
+        5 as id,
+        '' as string_trim_whitespace_true
+
     union all
-    select 6 as id, null as string
+
+    select
+        6 as id,
+        null as string_trim_whitespace_true
 
 )
 

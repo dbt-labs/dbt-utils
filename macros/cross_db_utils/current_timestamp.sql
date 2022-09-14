646e13acf83ec9388f79f692a402d50ba02bdf37
Jeremy Cohen <jeremy@dbtlabs.com>
2022-09-14 15:46:09 +0200

    Prepare current_timestamp macros for removal

--- macros/cross_db_utils/current_timestamp.sql
+++ macros/cross_db_utils/current_timestamp.sql
@@ -1,40 +1,19 @@
 {% macro current_timestamp() -%}
   {% do dbt_utils.xdb_deprecation_warning_without_replacement('current_timestamp', model.package_name, model.name) %}
-  {{ return(adapter.dispatch('current_timestamp', 'dbt_utils')()) }}
+  {{ return(adapter.dispatch('current_timestamp_backcompat', 'dbt')()) }}
 {%- endmacro %}
 
-{% macro default__current_timestamp() %}
-    current_timestamp::{{ type_timestamp() }}
-{% endmacro %}
-
-{% macro redshift__current_timestamp() %}
-    getdate()
-{% endmacro %}
-
-{% macro bigquery__current_timestamp() %}
-    current_timestamp
-{% endmacro %}
-
-
+/* {#
+    In the default case (not Snowflake or Postgres), this macro was just a passthrough to {{ dbt_utils.current_timestamp() }}.
+    Now it will be a passthrough to {{ dbt.current_timestamp_in_utc() }}, which will actually
+    use both {{ convert_timezone() }} + {{ dbt_utils.current_timestamp() }} to put the
+    timestamp in UTC.
+    
+    I believe this will yield the same result, or a more correct one (!)
+    -- if the current session is not in UTC -- but this needs testing.
+#} */
 
 {% macro current_timestamp_in_utc() -%}
   {% do dbt_utils.xdb_deprecation_warning_without_replacement('current_timestamp_in_utc', model.package_name, model.name) %}
-  {{ return(adapter.dispatch('current_timestamp_in_utc', 'dbt_utils')()) }}
+  {{ return(adapter.dispatch('current_timestamp_in_utc', 'dbt')()) }}
 {%- endmacro %}
-
-{% macro default__current_timestamp_in_utc() %}
-    {{ dbt_utils.current_timestamp() }}
-{% endmacro %}
-
-{% macro snowflake__current_timestamp_in_utc() %}
-    convert_timezone('UTC', {{ dbt_utils.current_timestamp() }})::{{ type_timestamp() }}
-{% endmacro %}
-
-{% macro postgres__current_timestamp_in_utc() %}
-    (current_timestamp at time zone 'utc')::{{ type_timestamp() }}
-{% endmacro %}
-
-{# redshift should use default instead of postgres #}
-{% macro redshift__current_timestamp_in_utc() %}
-    {{ return(dbt_utils.default__current_timestamp_in_utc()) }}
-{% endmacro %}

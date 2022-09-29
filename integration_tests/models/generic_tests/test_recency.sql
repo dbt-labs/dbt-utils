2d7321117d59b3ffb711ec2b3b53a0b8963e5ad8
Colin <colin.rogers@dbtlabs.com>
2022-09-29 11:13:32 -0700

    add recency test back

---
+++ integration_tests/models/generic_tests/test_recency.sql
@@ -0,0 +1,15 @@
+{% if target.type == 'postgres' %}
+
+select
+    1 as col1,
+    2 as col2,
+    {{ dbt_utils.date_trunc('day', dbt_utils.current_timestamp()) }} as today
+
+{% else %}
+
+select
+    1 as col1,
+    2 as col2,
+    cast({{ dbt_utils.date_trunc('day', dbt_utils.current_timestamp()) }} as datetime) as today
+    
+{% endif %}

c3be65cf39f12c87c1eb259bec5198dec2aa441e
Joel Labes <joel.labes@dbtlabs.com>
2022-10-04 18:09:22 +1300

    Remove references to retired macros in integration tests

--- integration_tests/models/generic_tests/test_recency.sql
+++ integration_tests/models/generic_tests/test_recency.sql
@@ -4,13 +4,13 @@
 select
     1 as col1,
     2 as col2,
-    {{ dbt_utils.date_trunc('day', current_timestamp()) }} as today
+    {{ date_trunc('day', current_timestamp()) }} as today
 
 {% else %}
 
 select
     1 as col1,
     2 as col2,
-    cast({{ dbt_utils.date_trunc('day', current_timestamp()) }} as datetime) as today
+    cast({{ date_trunc('day', current_timestamp()) }} as datetime) as today
     
 {% endif %}

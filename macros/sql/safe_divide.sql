6d010c400be2d92e9cc82d885c7c052cb793addb
deanna-minnick <41010575+deanna-minnick@users.noreply.github.com>
2022-10-11 13:30:42 -0400

    Revert "Feature/safe divide (#697)"
    This reverts commit f368cecca34a6488e14029d60d5116d7cc5fca51.

--- macros/sql/safe_divide.sql
+++
@@ -1,7 +0,0 @@
-{% macro safe_divide(numerator, denominator) -%}
-  {{ return(adapter.dispatch('safe_divide', 'dbt_utils')(numerator, denominator)) }}
-{%- endmacro %}
-
-{% macro default__safe_divide(numerator, denominator) %}
-    {{ numerator }} / nullif( {{ denominator }}, 0)
-{% endmacro %}

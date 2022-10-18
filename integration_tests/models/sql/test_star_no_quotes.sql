c2dfe3abeb84a1522e770391b04c7201a74d8c28
Doug Beatty <doug.beatty@dbtlabs.com>
2022-10-18 06:23:33 -0600

    Try to induce a test failure

--- integration_tests/models/sql/test_star_no_quotes.sql
+++ integration_tests/models/sql/test_star_no_quotes.sql
@@ -1,7 +1,7 @@
 with data as (
 
     select
-        {{ dbt_utils.star(from=ref('data_star_no_quotes'), quote_identifiers=False) | upper }}
+        {{ dbt_utils.star(from=ref('data_star_no_quotes'), quote_identifiers=True) | upper }}
 
     from {{ ref('data_star_no_quotes') }}
 
--- integration_tests/models/sql/test_star_quotes.sql
+++ integration_tests/models/sql/test_star_quotes.sql
@@ -1,7 +1,7 @@
 with data as (
 
     select
-        {{ dbt_utils.star(from=ref('data_star_quotes')) }}
+        {{ dbt_utils.star(from=ref('data_star_quotes'), quote_identifiers=False) }}
 
     from {{ ref('data_star_quotes') }}
 

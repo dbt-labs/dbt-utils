b08f7bb11c8025ff9bd5559040e80a0003e274ef
Joel Labes <joel.labes@dbtlabs.com>
2022-10-27 15:28:55 +1300

    Wrap xdb warnings in if execute block

--- macros/cross_db_utils/deprecated/xdb_deprecation_warning.sql
+++ macros/cross_db_utils/deprecated/xdb_deprecation_warning.sql
@@ -1,9 +1,13 @@
 {% macro xdb_deprecation_warning(macro, package, model) %}
-    {%- set error_message = "Warning: the `" ~ macro ~"` macro is now provided in dbt Core. It is no longer available in dbt_utils and backwards compatibility will be removed in a future version of the package. Use `dbt." ~ macro ~ "` instead. The " ~ package ~ "." ~ model ~ " model triggered this warning." -%}
-    {%- do exceptions.warn(error_message) -%}
+    {% if execute %}
+        {%- set error_message = "Warning: the `" ~ macro ~"` macro is now provided in dbt Core. It is no longer available in dbt_utils and backwards compatibility will be removed in a future version of the package. Use `dbt." ~ macro ~ "` instead. The " ~ package ~ "." ~ model ~ " model triggered this warning." -%}
+        {%- do exceptions.warn(error_message) -%}
+    {% endif %}
 {% endmacro %}
 
 {% macro xdb_deprecation_warning_without_replacement(macro, package, model) %}
-    {%- set error_message = "Warning: the `" ~ macro ~"` macro is deprecated and will be removed in a future version of the package, once equivalent functionality is implemented in dbt Core. The " ~ package ~ "." ~ model ~ " model triggered this warning." -%}
-    {%- do exceptions.warn(error_message) -%}
+    {% if execute %}
+        {%- set error_message = "Warning: the `" ~ macro ~"` macro is deprecated and will be removed in a future version of the package, once equivalent functionality is implemented in dbt Core. The " ~ package ~ "." ~ model ~ " model triggered this warning." -%}
+        {%- do exceptions.warn(error_message) -%}
+    {% endif %}
 {% endmacro %}

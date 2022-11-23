fd9de079af39c4f134bb9586343e3adde1c0dd24
Joel Labes <joel.labes@dbtlabs.com>
2022-11-23 14:05:25 +1300

    Add default value when no record found

--- macros/sql/get_query_results_as_single_value.sql
+++ macros/sql/get_query_results_as_single_value.sql
@@ -1,12 +1,12 @@
-{% macro get_query_results_as_single_value(query) %}
-    {{ return(adapter.dispatch('get_query_results_as_single_value', 'dbt_utils')(query)) }}
+{% macro get_query_results_as_single_value(query, default=none) %}
+    {{ return(adapter.dispatch('get_query_results_as_single_value', 'dbt_utils')(query, default)) }}
 {% endmacro %}
 
-{% macro default__get_query_results_as_single_value(query) %}
+{% macro default__get_query_results_as_single_value(query, default) %}
 
-{# This macro returns the (0, 0) record in a query #}
+{# This macro returns the (0, 0) record in a query, i.e. the first row of the first column #}
 
-    {%- call statement('get_query_result', fetch_result=True,auto_begin=false) -%}
+    {%- call statement('get_query_result', fetch_result=True, auto_begin=false) -%}
 
         {{ query }}
 
@@ -14,11 +14,17 @@
 
     {%- if execute -%}
 
-        {% set sql_result = load_result('get_query_result').table.columns[0].values()[0] %}
-    
+        {% set r = load_result('get_query_result').table.columns[0].values() %}
+        {% if r | length == 0 %}
+            {% do print('Query `' ~ query ~ '` returned no rows. Using the default value: ' ~ default) %}
+            {% set sql_result = default %}
+        {% else %}
+            {% set sql_result = r[0] %}
+        {% endif %}
+        
     {%- else -%}
     
-        {% set sql_result = "" %}
+        {% set sql_result = default %}
     
     {%- endif -%}
 

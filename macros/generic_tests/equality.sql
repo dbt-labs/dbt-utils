80a16a9f566479e306f157d167e31ed6b6924ab4
bruno <bruno.lopes@plentific.com>
2022-11-24 10:38:22 +0000

    add exclude columns to equality test

--- macros/generic_tests/equality.sql
+++ macros/generic_tests/equality.sql
@@ -1,8 +1,8 @@
-{% test equality(model, compare_model, compare_columns=None) %}
-  {{ return(adapter.dispatch('test_equality', 'dbt_utils')(model, compare_model, compare_columns)) }}
+{% test equality(model, compare_model, compare_columns=None, exclude_columns=None) %}
+  {{ return(adapter.dispatch('test_equality', 'dbt_utils')(model, compare_model, compare_columns, exclude_columns)) }}
 {% endtest %}
 
-{% macro default__test_equality(model, compare_model, compare_columns=None) %}
+{% macro default__test_equality(model, compare_model, compare_columns=None, exclude_columns=None) %}
 
 {% set set_diff %}
     count(*) + coalesce(abs(
@@ -30,6 +30,21 @@ information schema — this allows the model to be an ephemeral model
 {%- if not compare_columns -%}
     {%- do dbt_utils._is_ephemeral(model, 'test_equality') -%}
     {%- set compare_columns = adapter.get_columns_in_relation(model) | map(attribute='quoted') -%}
+{%- endif -%}
+
+{%- if exclude_columns -%}
+    {#-- Lower case exclude columns for easier comparison --#}
+    {%- set exclude_columns = exclude_columns | map("lower") | list %}
+
+    {%- set include_columns = [] %}
+    {%- for column in compare_columns -%}
+        {#-- Compare columns retrieved via the adapter wrapper are quoted --#}
+        {%- if column | lower | trim('"') not in exclude_columns -%}
+            {% do include_columns.append(column) %}
+        {%- endif %}
+    {%- endfor %}
+
+    {%- set compare_columns = include_columns %}
 {%- endif -%}
 
 {% set compare_cols_csv = compare_columns | join(', ') %}

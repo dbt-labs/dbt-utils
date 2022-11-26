2199ae4ca2b9e4b7cff90cf4e87980d48b3449f9
Joel Labes <joel.labes@dbtlabs.com>
2022-11-27 09:26:57 +1300

    cast source as timestamp

--- integration_tests/models/generic_tests/recency_time_included.sql
+++ integration_tests/models/generic_tests/recency_time_included.sql
@@ -1,4 +1,4 @@
 select
     1 as col1,
     2 as col2,
-    {{ dbt.dateadd('hour', -23, dbt.current_timestamp()) }} as created_at
+    cast({{ dbt.dateadd('hour', -23, dbt.current_timestamp()) }} as {{ dbt.type_timestamp() }}) as created_at

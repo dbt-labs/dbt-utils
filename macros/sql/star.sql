5a50552d62e2fa139c1feffb1668e82ebab8bcb9
Joel Labes <joel.labes@dbtlabs.com>
2022-11-29 15:20:21 +1300

    Code review: return a * in compile mode

--- macros/sql/star.sql
+++ macros/sql/star.sql
@@ -8,13 +8,23 @@
 
     {#-- Prevent querying of db in parsing mode. This works because this macro does not create any new refs. #}
     {%- if not execute -%}
-        {{ return('*') }}
+        {% do return('*') %}
     {%- endif -%}
 
     {% set cols = dbt_utils.get_filtered_columns_in_relation(from, except) %}
 
     {%- if cols|length <= 0 -%}
-      {{- return(dbt.string_literal("no columns returned from star() macro") ~ " as outcome_msg") -}}
+        {% if flags.WHICH == 'compile' %}
+            {% set response %}
+*
+/* No columns were returned. Maybe the relation doesn't exist yet 
+or all columns were excluded. This star is only output during  
+dbt compile, and exists to keep SQLFluff happy. */
+            {% endset %}
+            {% do return(response) %}
+        {% else %}
+            {% do return("/* no columns returned from star() macro */") %}
+        {% endif %}
     {%- else -%}
         {%- for col in cols %}
             {%- if relation_alias %}{{ relation_alias }}.{% else %}{%- endif -%}

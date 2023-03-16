6ead71315370dd349a579f569c837cf81a548dc4
Joel Labes <joel.labes@dbtlabs.com>
2023-03-16 15:45:41 +1300

    Update ci command

--- run_test.sh
+++ run_test.sh
@@ -14,15 +14,5 @@ export DBT_PROFILES_DIR=.
 # Show the location of the profiles directory and test the connection
 dbt debug --target $1
 
-_models=""
-_seeds="--full-refresh"
-if [[ ! -z $2 ]]; then _models="--models $2"; fi
-if [[ ! -z $3 ]]; then _seeds="--select $3 --full-refresh"; fi
-
 dbt deps --target $1 || exit 1
-dbt seed --target $1 $_seeds || exit 1
-if [ $1 == 'redshift' ]; then
-    dbt run -x -m test_insert_by_period --full-refresh --target redshift || exit 1
-fi
-dbt run -x --target $1 $_models || exit 1
-dbt test -x --target $1 $_models || exit 1
+dbt build --target $1 --full-refresh || exit 1

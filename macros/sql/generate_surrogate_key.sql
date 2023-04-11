a48c90623f572ed7b08104c1de1cee323e206b42
Josh Devlin <joshuadevlin@gmail.com>
2023-04-11 13:10:51 +1000

    Add optional override arg for behavior of generate_surrogate_key()

--- macros/sql/generate_surrogate_key.sql
+++ macros/sql/generate_surrogate_key.sql
@@ -1,11 +1,19 @@
-{%- macro generate_surrogate_key(field_list) -%}
-    {{ return(adapter.dispatch('generate_surrogate_key', 'dbt_utils')(field_list)) }}
+{%- macro generate_surrogate_key(field_list, treat_nulls_as_empty_strings=None) -%}
+    {{ return(adapter.dispatch('generate_surrogate_key', 'dbt_utils')(field_list, treat_nulls_as_empty_strings=None)) }}
 {% endmacro %}
 
-{%- macro default__generate_surrogate_key(field_list) -%}
+{%- macro default__generate_surrogate_key(field_list, treat_nulls_as_empty_strings=None) -%}
 
-{%- if var('surrogate_key_treat_nulls_as_empty_strings', False) -%}
+
+{# check for the override variable #}
+{%- if treat_nulls_as_empty_strings -%}
     {%- set default_null_value = "" -%}
+{%- elif not treat_nulls_as_empty_strings -%}
+    {%- set default_null_value = '_dbt_utils_surrogate_key_null_' -%}
+{# Check project config variable #}
+{%- elif var('surrogate_key_treat_nulls_as_empty_strings', False) -%}
+    {%- set default_null_value = "" -%}
+{# fallback to default behavior #}
 {%- else -%}
     {%- set default_null_value = '_dbt_utils_surrogate_key_null_' -%}
 {%- endif -%}

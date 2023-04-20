9d145b9ff6bffa04f2c3cc238bf3f816992480cf
JoshuaHuntley <JoshuaHuntley@users.noreply.github.com>
2023-04-20 12:38:11 -0700

    Change requests

--- macros/generic_tests/at_least_one.sql
+++ macros/generic_tests/at_least_one.sql
@@ -4,35 +4,28 @@
 
 {% macro default__test_at_least_one(model, column_name, group_by_columns) %}
 
-{#- Ensure that column_name is not in the group by list or the database will throw an ambiguous column error #}
-{%- set group_by_cols_filtered = [] %}
-{% for column in group_by_columns %}
-  {%- if column|upper != column_name|upper %}
-      {%- do group_by_cols_filtered.append(column) %}
-  {% endif -%}
-{% endfor %}
-
-{% if group_by_cols_filtered|length() > 0 %}
-  {% set select_gb_cols = group_by_cols_filtered|join(' ,') + ', ' %}
-  {% set groupby_gb_cols = 'group by ' + group_by_cols_filtered|join(',') %}
+{% if group_by_columns|length() > 0 %}
+  {% set select_gb_cols = group_by_columns|join(' ,') + ', ' %}
+  {% set groupby_gb_cols = 'group by ' + group_by_columns|join(',') %}
 {% endif %}
 
 select *
 from (
+    with pruned_rows as (
+      select
+        {{select_gb_cols}}
+        {{ column_name }}
+      from {{ model }}
+      where {{ column_name }} is not null
+      limit 1
+    )
     select
         {# In TSQL, subquery aggregate columns need aliases #}
         {# thus: a filler col name, 'filler_column' #}
       {{select_gb_cols}}
       count({{ column_name }}) as filler_column
 
-    from (
-          select
-            {{select_gb_cols}}
-            {{ column_name }}
-          from {{ model }}
-          where {{ column_name }} is not null
-          limit 1
-          ) pruned_rows
+    from pruned_rows
 
     {{groupby_gb_cols}}
 

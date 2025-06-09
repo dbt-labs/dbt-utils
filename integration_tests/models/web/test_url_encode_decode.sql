02c9d9f01829f95da93d95aa5200e98051f40550
Damir Mustafic <damir.m83@gmail.com>
2025-06-09 22:46:07 +0200

    Fixed integration test

--- integration_tests/models/web/test_url_encode_decode.sql
+++ integration_tests/models/web/test_url_encode_decode.sql
@@ -4,18 +4,24 @@ test_data as (
 
     select * from {{ ref('data_url_encode_decode') }}
 
+),
+
+final as (
+
+    select
+        {{ dbt_utils.url_encode('decoded') }} as actual,
+        encoded as expected
+
+    from test_data
+
+    union all
+
+    select
+        {{ dbt_utils.url_decode('encoded') }} as actual,
+        decoded as expected
+
+    from test_data
+
 )
 
-select
-    {{ url_encode('decoded') }} as actual,
-    encoded as expected
-
-from test_data
-
-union all
-
-select
-    {{ url_decode('encoded') }} as actual,
-    decoded as expected
-
-from test_data
+select * from final

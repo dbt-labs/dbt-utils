87b3cabb38841d0ed247b2a7bc1a0ade2f31b244
Harshvardhan Joshi <harshvardhan.j@huspy.io>
2025-12-05 11:21:48 +0400

    fix: Render relations to avoid including time filters in source_column_name

--- macros/sql/union.sql
+++ macros/sql/union.sql
@@ -100,7 +100,7 @@
             select
 
                 {%- if source_column_name is not none %}
-                cast({{ dbt.string_literal(relation) }} as {{ dbt.type_string() }}) as {{ source_column_name }},
+                cast({{ dbt.string_literal(relation.render()) }} as {{ dbt.type_string() }}) as {{ source_column_name }},
                 {%- endif %}
 
                 {% for col_name in ordered_column_names -%}

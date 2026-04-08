17cbdabd9f0608ea2f2cb3fb5a45b794c03dc31e
Hitendrasinhdata7 <hitendrasinh.data7@gmail.com>
2026-04-08 11:12:02 +0100

    fix: support dbt Core 1.10+ arguments keyword in unique_combination_of_columns
    - Add kwargs.get() fallback for combination_of_columns and quote_columns
    - Handle nested 'args'/'arguments' keys for future compatibility
    - Add integration tests for both old and new syntax

--- macros/generic_tests/unique_combination_of_columns.sql
+++ macros/generic_tests/unique_combination_of_columns.sql
@@ -1,26 +1,27 @@
-{% test unique_combination_of_columns(model, combination_of_columns, quote_columns=false) %}
-  {{ return(adapter.dispatch('test_unique_combination_of_columns', 'dbt_utils')(model, combination_of_columns, quote_columns)) }}
-{% endtest %}
-
 {% macro default__test_unique_combination_of_columns(model, combination_of_columns, quote_columns=false) %}
 
+-- Support dbt Core 1.10+ 'arguments:' syntax (kwargs)
 {% set combination_of_columns = kwargs.get('combination_of_columns', combination_of_columns) %}
 {% set quote_columns = kwargs.get('quote_columns', quote_columns) %}
 
-{% if not quote_columns %}
-    {%- set column_list=combination_of_columns %}
-{% elif quote_columns %}
-    {%- set column_list=[] %}
-        {% for column in combination_of_columns -%}
-            {% do column_list.append( adapter.quote(column) ) %}
-        {%- endfor %}
-{% else %}
-    {{ exceptions.raise_compiler_error(
-        "`quote_columns` argument for unique_combination_of_columns test must be one of [True, False] Got: '" ~ quote ~"'.'"
-    ) }}
+-- Also handle possible nested 'args' or 'arguments' keys for future compatibility
+{% if kwargs.get('args') is mapping %}
+    {% set combination_of_columns = kwargs.args.get('combination_of_columns', combination_of_columns) %}
+    {% set quote_columns = kwargs.args.get('quote_columns', quote_columns) %}
+{% elif kwargs.get('arguments') is mapping %}
+    {% set combination_of_columns = kwargs.arguments.get('combination_of_columns', combination_of_columns) %}
+    {% set quote_columns = kwargs.arguments.get('quote_columns', quote_columns) %}
 {% endif %}
 
-{%- set columns_csv=column_list | join(', ') %}
+{% if not quote_columns %}
+    {%- set column_list = combination_of_columns %}
+{% elif quote_columns %}
+    {%- set column_list = quote_columns(combination_of_columns) %}
+{% else %}
+    {{ exceptions.raise_compiler_error("Invalid 'quote_columns' argument provided. Got '" ~ quote_columns ~ "' but expected boolean True/False") }}
+{% endif %}
+
+{%- set columns_csv = column_list | join(', ') %}
 
 with validation_errors as (
     select
